f49ad18bca09927e4393d1737bb8d30151739c55
Maciej Kucharz <maciek@kucharz.net>
2015-05-25 15:57:13 +0200

    Few more things.

--- check_tests.py
+++ check_tests.py
@@ -9,16 +9,25 @@ file_template = """jest.dontMock("../%(code_file)s");
 
 describe("%(code_file)s", function() {
   it("testing component", function() {
-    require('../%(code_file)s')
+
+    var React = require('react/addons');
+    var TestUtils = React.addons.TestUtils;
+    var %(class_name)s = require('../%(code_file)s');
   });
 });"""
 
 def create_test_file(test_path, code_file):
+    cf = code_file.replace('.jsx', '')
+    class_name = cf.replace('.react', '')
+    directory = os.path.dirname(test_path)
     try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         test_file = open(test_path, 'w+')
-        test_file.write(file_template % {'code_file': code_file})
+        test_file.write(file_template % {'code_file': cf, 'class_name': class_name})
         test_file.close()
-    except:
+    except Exception as e:
+        print e.message
         print "Can't create a file: %s!" % test_path
         sys.exit(1)
 
@@ -29,10 +38,10 @@ def check_tests(files):
         full_path = os.path.join(os.path.realpath('.'), file_path) # Real path
         directory, filename = full_path.rsplit('/', 1) # File directory and file full name
 
-        filename_name, filename_suffix = filename.split('.')
+        filename_name, filename_suffix = filename.rsplit('.', 1)
         filename_suffix = 'js' # Tests are always in JS
 
-        test_name = '%s_test.%s' % (filename_name, filename_suffix)
+        test_name = '%s.test.%s' % (filename_name, filename_suffix)
         test_full_path = os.path.join(os.path.realpath('.'), directory, '__tests__', test_name)
 
         if not os.path.exists(test_full_path):
