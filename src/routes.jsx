58197c11a182ea1798ce31340c30c48d9c067f66
Daniel Kopka <daniel.kopka@syncano.com>
2015-05-28 15:23:47 +0200

    [SYNGUI-661] inestances, data objects, classes

--- src/routes.jsx
+++ src/routes.jsx
@@ -15,8 +15,9 @@ var React                       = require('react'),
     AccountPasswordReset        = require('./apps/Account/AccountPasswordReset.react'),
     AccountPasswordResetConfirm = require('./apps/Account/AccountPasswordResetConfirm.react'),
 
-    // Instances
     Instances                   = require('./apps/Instances/Instances.react'),
+    Classes                     = require('./apps/Classes/Classes.react'),
+    DataObjects                 = require('./apps/DataObjects/DataObjects.react'),
 
     // Examples
     Examples                    = require('./examples/Examples.react');
@@ -31,7 +32,9 @@ module.exports = (
     <Route name="password-reset-confirm" handler={AccountPasswordResetConfirm} path="/password/reset/:uid-:token" />
 
     <Route name="dashboard" handler={Dashboard} path="/">
-      <Route name="instance-list" handler={Instances} path="/instances" />
+      <Route name="instances" handler={Instances} path="/instances" />
+      <Route name="classes" handler={Classes} path="/instances/:instanceName/classes" />
+      <Route name="data-objects" handler={DataObjects} path="/instances/:instanceName/objects" />
     </Route>
 
     <Route name="examples" handler={Examples}/>
