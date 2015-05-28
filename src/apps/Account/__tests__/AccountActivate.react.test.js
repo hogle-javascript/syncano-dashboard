cc8a147972e6c8c6144485064180f13e078a7283
Daniel Kopka <daniel.kopka@syncano.com>
2015-05-28 14:40:03 +0200

    [SYNGUI-661] few utils and tests

---
+++ src/apps/Account/__tests__/AccountActivate.react.test.js
@@ -0,0 +1,16 @@
+jest.dontMock('../AccountActivate.react');
+jest.dontMock('../../../utils/ReactRouterContext');
+jest.dontMock('../../../utils/objectAssign');
+
+describe('AccountActivate.react', function () {
+  it('testing component', function () {
+
+    var React               = require('react/addons'),
+        TestUtils           = React.addons.TestUtils,
+        AccountActivate     = require('../AccountActivate.react'),
+        ReactRouterContext  = require('../../../utils/ReactRouterContext'),
+        Subject             = ReactRouterContext(AccountActivate, {params: {uid:1, token: 2}}),
+        AccountActivateView = TestUtils.renderIntoDocument(<Subject />);
+
+  });
+});
---
+++ src/apps/Account/__tests__/AccountPasswordResetConfirm.react.test.js
@@ -0,0 +1,15 @@
+jest.dontMock('../AccountPasswordResetConfirm.react');
+jest.dontMock('../../../utils/ReactRouterContext');
+jest.dontMock('../../../utils/objectAssign');
+
+describe('AccountPasswordResetConfirm.react', function () {
+  it('testing component', function () {
+
+    var React                           = require('react/addons'),
+        TestUtils                       = React.addons.TestUtils,
+        AccountPasswordResetConfirm     = require('../AccountPasswordResetConfirm.react'),
+        ReactRouterContext              = require('../../../utils/ReactRouterContext'),
+        Subject                         = ReactRouterContext(AccountPasswordResetConfirm, {params: {uid:1, token: 2}}),
+        AccountPasswordResetConfirmView = TestUtils.renderIntoDocument(<Subject />);
+  });
+});
