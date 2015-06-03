998bdee4772127434eb0600d54ef0243f34c912a
Daniel Kopka <daniel.kopka@syncano.com>
2015-06-03 15:52:02 +0200

    [SYNGUI-668] password signup

--- src/apps/Account/AuthActions.js
+++ src/apps/Account/AuthActions.js
@@ -2,10 +2,34 @@ var alt     = require('../../alt');
     Syncano = require('../../lib/syncano4');
 
 
+// TODO: make this code more DRY
 function AuthActions() {
 
 };
 
+// TODO: Change this Source http://alt.js.org/docs/async/
+AuthActions.prototype.passwordSignUp = function (payload) {
+  this.dispatch(payload);
+
+  new Syncano().Accounts.create({
+      email: payload.email,
+      password: payload.password
+    }).then(
+      this.actions.passwordSignUpSucceeded,
+      this.actions.passwordSignUpFailed
+    );
+
+};
+
+AuthActions.prototype.passwordSignUpSucceeded = function (payload) {
+  this.dispatch(payload);
+};
+
+AuthActions.prototype.passwordSignUpFailed = function (error) {
+  this.dispatch(error);
+};
+
+// TODO: Change this Source http://alt.js.org/docs/async/
 AuthActions.prototype.passwordSignIn = function (payload) {
   this.dispatch(payload);
 
--- src/apps/Account/AuthStore.js
+++ src/apps/Account/AuthStore.js
@@ -2,8 +2,9 @@ var alt         = require('../../alt'),
     AuthActions = require('./AuthActions');
 
 
+// TODO: make this code more DRY
 function AuthStore() {
-  this.token     = sessionStorage.getItem('token');
+  this.token     = sessionStorage.getItem('token') || null;
   this.user      = null;
   this.errors    = {};
   this.email     = null;
@@ -14,6 +15,9 @@ function AuthStore() {
       handlePasswordSignIn: AuthActions.PASSWORD_SIGN_IN,
       handlePasswordSignInSucceeded: AuthActions.PASSWORD_SIGN_IN_SUCCEEDED,
       handlePasswordSignInFailed: AuthActions.PASSWORD_SIGN_IN_FAILED,
+      handlePasswordSignUp: AuthActions.PASSWORD_SIGN_UP,
+      handlePasswordSignUpSucceeded: AuthActions.PASSWORD_SIGN_UP_SUCCEEDED,
+      handlePasswordSignUpFailed: AuthActions.PASSWORD_SIGN_UP_FAILED,
       handleLogout: AuthActions.LOGOUT,
   });
 };
@@ -37,6 +41,44 @@ AuthStore.prototype.handlePasswordSignInSucceeded = function (payload) {
 };
 
 AuthStore.prototype.handlePasswordSignInFailed = function (payload) {
+  this.token     = null;
+  this.user      = null;
+  this.errors    = {};
+  this.canSubmit = true;
+
+  sessionStorage.removeItem('token');
+
+
+  if (typeof payload === 'string') {
+    this.errors.feedback = payload;
+  } else {
+    for (var field in payload) {
+      this.errors[field] = payload[field];
+    }
+  }
+};
+
+AuthStore.prototype.handlePasswordSignUp = function (payload) {
+  this.errors    = {};
+  this.email     = payload.email;
+  this.password  = payload.password;
+  this.canSubmit = false;
+};
+
+AuthStore.prototype.handlePasswordSignUpSucceeded = function (payload) {
+  this.token     = payload.account_key || null;
+  this.user      = payload;
+  this.errors    = {};
+  this.email     = null;
+  this.password  = null;
+  this.canSubmit = true;
+
+  if (this.token !== null) {
+    sessionStorage.setItem('token', this.token);
+  }
+};
+
+AuthStore.prototype.handlePasswordSignUpFailed = function (payload) {
   this.token     = null;
   this.user      = null;
   this.errors    = {};
