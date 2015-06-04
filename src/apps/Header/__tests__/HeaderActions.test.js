e162a131c35f2271e7ff127f74ba7778a463410a
Daniel Kopka <daniel.kopka@syncano.com>
2015-06-04 18:22:02 +0200

    [SYNGUI-672] missing fiels

---
+++ src/apps/Header/__tests__/HeaderActions.test.js
@@ -0,0 +1,10 @@
+jest.dontMock("../HeaderActions");
+
+describe("HeaderActions", function() {
+  it("testing component", function() {
+
+    var React = require('react/addons');
+    var TestUtils = React.addons.TestUtils;
+    var HeaderActions = require('../HeaderActions');
+  });
+});
---
+++ src/apps/Header/__tests__/HeaderStore.test.js
@@ -0,0 +1,10 @@
+jest.dontMock("../HeaderStore");
+
+describe("HeaderStore", function() {
+  it("testing component", function() {
+
+    var React = require('react/addons');
+    var TestUtils = React.addons.TestUtils;
+    var HeaderStore = require('../HeaderStore');
+  });
+});
