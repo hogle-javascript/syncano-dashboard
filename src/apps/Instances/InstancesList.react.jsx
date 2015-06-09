b370da55e5ea930452fd9813ed6646559a1ad2f0
Maciej Kucharz <maciek@kucharz.net>
2015-06-09 22:53:17 +0200

    Very first implementation of instances: listing and creating - we have to rethink UX for tables to move forward.

--- src/apps/Instances/InstancesList.react.jsx
+++ src/apps/Instances/InstancesList.react.jsx
@@ -1,109 +1,139 @@
-var React = require('react');
-var Moment = require('moment');
-var classNames = require('classnames');
+var React  = require('react'),
+    Reflux = require('reflux'),
+    Router = require('react-router'),
 
-var ListItem = require('./InstancesListItem.react');
-//var ListItemColumns = require('./ListItemColumns.react');
-var ListItemEmpty = require('../../common/Lists/ListItemEmpty.react');
+    // Utils
+    HeaderMixin       = require('../Header/HeaderMixin'),
+    ButtonActionMixin = require('../../mixins/ButtonActionMixin'),
+
+    // Stores and Actions
+    SessionStore     = require('../Session/SessionStore'),
+    SessionActions   = require('../Session/SessionActions'),
+    InstancesActions = require('./InstancesActions'),
+    InstancesStore   = require('./InstancesStore'),
+
+    // Components
+    mui              = require('material-ui'),
+    Dialog           = mui.Dialog,
+    ListContainer    = require('../../common/Lists/ListContainer.react'),
+    Item             = require('../../common/ColumnList/Item.react'),
+    Column           = require('../../common/ColumnList/ItemColumn.react'),
+    Header           = require('../../common/ColumnList/Header.react'),
+    ColNameDesc      = require('../../common/ColumnList/ColNameDesc.react'),
+    ColName          = require('../../common/ColumnList/ColName.react'),
+    FabList          = require('../../common/Fab/FabList.react');
 
 
 module.exports = React.createClass({
 
-  displayName: 'List',
+  displayName: 'InstancesList',
 
-  propTypes: {
-    list: React.PropTypes.object.isRequired,
-    handleItemMenuClick: React.PropTypes.func.isRequired,
-  },
+  mixins: [
+    Reflux.connect(InstancesStore),
+    ButtonActionMixin,
+    HeaderMixin,
+    Router.State,
+    Router.Navigation,
+    //React.addons.LinkedStateMixin,
+    //ValidationMixin,
+  ],
 
-  getDefaultProps: function () {
+  getInitialState: function() {
     return {
-      data: []
+      listType: this.props.listType
     }
   },
 
-  handleClick: function (item) {
-    //var instanceName = InstanceStore.getRouteParams().instanceName;
-    //if (this.props.list.contentType === "instances") {
-    //  window.location.href = '#instances/' + item.id;
-    //} else if (this.props.list.contentType === "codeboxes") {
-    //  window.location.href = '#instances/' + instanceName + '/codeboxes/' + item.id;
-    //} else if (this.props.list.contentType === "triggers") {
-    //  window.location.href = '#instances/' + instanceName + '/triggers/' + item.id;
-    //} else if (this.props.list.contentType === "schedules") {
-    //  window.location.href = '#instances/' + instanceName + '/schedules/' + item.id;
-    //} else if (this.props.list.contentType === "webhooks") {
-    //  window.location.href = '#instances/' + instanceName + '/webhooks/' + item.id;
-    //} else if (this.props.list.contentType === "traces") {
-    //  ViewActions.expandListItem(item);
-    //}
+  componentWillMount: function() {
   },
 
-  //getCodeBoxName: function (item) {
-  //  var codeBoxes = InstanceStore._getCodeBoxes();
-  //  var codeBoxName;
-  //  codeBoxes.forEach(function (codeBox) {
-  //    if (codeBox.id === item.data.codebox) {
-  //      codeBoxName = codeBox.data.name
-  //    }
-  //  });
-  //  return codeBoxName;
-  //},
+  // List
+  handleItemIconClick: function (id, state) {
+    var checkedItemNumber;
+    if (state) {
+      checkedItemNumber = ++this.state.checkedItemNumber;
+    } else {
+      checkedItemNumber = --this.state.checkedItemNumber;
+    }
 
-  handleItemMenuClick: function (action) {
-    // We need to add here information about list we are sorting/changing/view etc.
-    action['list'] = this.props.list.uuid;
-    this.props.handleItemMenuClick(action);
+    this.setState({
+      checkingState: checkedItemNumber > 0,
+      checkedItemNumber: checkedItemNumber,
+    });
+    console.log('checked', checkedItemNumber)
+  },
+
+  handleItemClick: function(instanceName) {
+    // Redirect to main instance screen
+    SessionActions.setInstance(instanceName);
+    this.transitionTo('instance', {instanceName: instanceName});
+  },
+
+  getColumns: function() {
+    return [
+      {'name': this.props.name, space: 1, style: {fontSize: '20px'}},
+      {'name': 'Name', space: 5},
+      {'name': 'Description', space: 4},
+      {'name': 'Created', space: 2},
+    ];
+  },
+
+  generateItem: function (item) {
+
+    return (
+      <Item key={item.name}>
+        <Column grid="1">
+          <CheckIcon
+            id          = {item.name}
+            icon        = {item.metadata.icon || 'adjust'}
+            background  = {item.metadata.color || 'green'}
+            width       = '40px'
+            handleClick = {this.handleItemIconClick}
+            />
+        </Column>
+        <Column grid="5">
+          <ColName id={item.name} handleClick={this.handleItemClick}>
+            {item.name}
+          </ColName>
+        </Column>
+        <Column grid="4">
+          <span>{item.description}</span>
+        </Column>
+        <Column grid="2">
+          <span>{item.created_at}</span>
+        </Column>
+      </Item>
+    )
+  },
+
+  getList: function () {
+    var instances = this.state.instances[this.state.listType] || [];
+
+    var items = instances.map(function (item) {
+      return this.generateItem(item)
+    }.bind(this));
+
+    if (items.length > 0) {
+      // TODO: Fix this dirty hack, that should be done in store by sorting!
+      items.reverse();
+      return items;
+    }
+    return [<Item key="empty">Empty Item</Item>];
   },
 
   render: function () {
-
-    console.log('Rendering InstanceList');
-
-
-    var list = this.props.list;
-    var data = list.data;
-
-    var expandable;
-    var expanded;
-    var color;
-    var info;
-
-
-    //var config = ConfigStore.getConfig();
-    var contentType = this.props.list.contentType;
-
-    //var view = ViewsStore.getView(contentType);
-    //var icon = view.icon;
-
-
-    console.log('Number of items', data.length);
-
-    var items = [];
-    if (data.length > 0) {
-      console.log('Iterate over items', data);
-      items = Object.keys(data).map(function (item, i) {
-        return (<ListItem
-          key={i}
-          item={data[item]}
-          actions={list.actions}
-          handleItemMenuClick={this.handleItemMenuClick}
-          style={list.viewMode}
-          avatarStyle={this.props.list.avatarStyle}/>)
-      }.bind(this));
-    } else if (data.length === 0) {
-      items = (<ListItemEmpty icon={this.props.list.emptyIcon}
-                              text={this.props.list.emptyText}/>)
-    }
-
-
-    var cssClasses = classNames('list', 'items-list', 'view-' + list.viewMode);
-
-    console.log("List items", items);
     return (
-      <div className={cssClasses}>
-        {items}
-      </div>
+        <ListContainer>
+          <Header
+            checkedItemsNumber = {this.state.checkedItemNumber}
+            columns            = {this.getColumns()}>
+            <MaterialIcon name="group_add" handleClick={this.dummyClick} />
+            <MaterialIcon name="home" handleClick={this.dummyClick} />
+          </Header>
+          <List viewMode={this.props.viewMode}>
+            {this.getList()}
+          </List>
+        </ListContainer>
     );
   }
 
---
+++ src/apps/Instances/InstancesStore.js
@@ -0,0 +1,67 @@
+var Reflux = require('reflux'),
+
+    SessionStore     = require('../Session/SessionStore'),
+    InstancesActions = require('./InstancesActions');
+
+
+var InstancesStore = Reflux.createStore({
+  listenables: InstancesActions,
+
+  getInitialState: function () {
+    return {
+      errors: {},
+      instances: {
+        myInstances: [],
+        otherInstances: [],
+      },
+    }
+  },
+
+  init: function () {
+
+    this.data = {
+      instances: {
+        myInstances: [],
+        otherInstances: [],
+      },
+      checkedItemNumber: 0,
+      canSubmit: true,
+      errors: {},
+    };
+
+    // We want to know when we are ready to download data for this store,
+    // it depends on instance we working on
+    this.listenTo(SessionStore, this.refreshData);
+  },
+
+  refreshData: function () {
+    console.debug('InstancesStore::refreshData');
+    InstancesActions.getInstances();
+  },
+
+  onGetInstancesCompleted: function(instances) {
+    console.debug('InstancesStore::onGetInstanesCompleted');
+
+    var data = this.data;
+    data.instances.myInstances = [];
+    data.instances.otherInstances = [];
+
+    Object.keys(instances).map(function(item) {
+      if (instances[item].owner.email === SessionStore.getMyEmail()) {
+
+        data.instances.myInstances.push(instances[item]);
+      } else {
+        data.instances.otherInstances.push(instances[item]);
+      }
+    });
+    this.trigger(this.data);
+  },
+
+  onCreateInstanceCompleted: function(payload) {
+    console.debug('InstancesStore::onCreateInstanceCompleted');
+    this.refreshData();
+  }
+
+});
+
+module.exports = InstancesStore;
