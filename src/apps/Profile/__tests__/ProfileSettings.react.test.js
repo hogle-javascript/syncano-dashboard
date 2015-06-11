c57802d551c33744feb6eeecad8a652b06feaaf6
Daniel Kopka <daniel.kopka@syncano.com>
2015-06-11 14:01:56 +0200

    [SYNGUI-709] new tests

---
+++ src/apps/Profile/__tests__/ProfileSettings.react.test.js
@@ -0,0 +1,11 @@
+jest.dontMock("../ProfileSettings.react");
+
+describe("ProfileSettings.react", function () {
+  it("testing component", function () {
+
+    var React        = require('react/addons'),
+        TestUtils    = React.addons.TestUtils;
+
+
+  });
+});
