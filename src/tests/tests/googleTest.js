8521c37933d7685f6d2cff3332489e2bad367fbb
Adam Wardecki <adam@Adams-MacBook-Pro.local>
2015-06-12 14:13:54 +0200

    removed dummy tests

--- src/tests/tests/googleTest.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "Demo test Google" : function (client) {
-        client
-            .page.google().goToGoogle()
-            .page.google().searchForNightwatch()
-            .page.google().verifyNightwatchIsFound()
-            .end();
-    }
-};
