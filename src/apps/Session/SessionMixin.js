5e9ef97b802de9ed57a694647cbca5374dfb27b1
Daniel Kopka <daniel.kopka@syncano.com>
2015-06-14 14:09:54 +0200

    [SYNGUI-730] naming

--- src/apps/Session/SessionMixin.js
+++ src/apps/Session/SessionMixin.js
@@ -4,14 +4,14 @@ var SessionStore = require('./SessionStore');
 var SessionMixin = {
   _onSessionStatusChange: function (Session) {
     if (Session.isReady()) {
-      this.sessionIsReady(Session);
+      this.onSessionIsReady(Session);
       this._unsubscribeSessionStore();
     }
   },
 
   componentDidMount: function() {
-    if (this.sessionIsReady === undefined || typeof this.sessionIsReady !== 'function') {
-      throw Error('Invalid `sessionIsReady` type');
+    if (this.onSessionIsReady === undefined || typeof this.onSessionIsReady !== 'function') {
+      throw Error('Invalid `onSessionIsReady` type');
     }
     this._unsubscribeSessionStore = SessionStore.listen(this._onSessionStatusChange);
     this._onSessionStatusChange(SessionStore);
