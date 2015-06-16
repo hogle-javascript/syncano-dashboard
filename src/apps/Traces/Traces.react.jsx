8810f1cda33634b324e4048f70680e9237981442
Maciej Kucharz <maciek@kucharz.net>
2015-06-16 21:56:44 +0200

    Traces as a independent app - for now usable only for codebox.

---
+++ src/apps/Traces/Traces.react.jsx
@@ -0,0 +1,53 @@
+var React             = require('react'),
+    Reflux            = require('reflux'),
+    Router            = require('react-router'),
+
+    // Utils
+    HeaderMixin       = require('../Header/HeaderMixin'),
+    InstanceTabsMixin = require('../../mixins/InstanceTabsMixin'),
+
+    // Stores and Actions
+    TracesActions     = require('./TracesActions'),
+    TracesStore       = require('./TracesStore'),
+
+    // Components
+    Container         = require('../../common/Container/Container.react'),
+
+    // Local components
+    TracesList        = require('./TracesList.react');
+
+
+module.exports = React.createClass({
+
+  displayName: 'Traces',
+
+  mixins: [
+    Router.State,
+    Router.Navigation,
+
+    Reflux.connect(TracesStore),
+    HeaderMixin,
+    InstanceTabsMixin
+  ],
+
+  componentWillMount: function() {
+    var codeboxId = this.getParams().codeboxId;
+
+    TracesActions.setCurrentObjectId(codeboxId);
+    this.setState({
+      objectId : codeboxId
+    });
+    TracesStore.refreshData();
+  },
+
+  render: function () {
+    return (
+      <Container>
+        <TracesList
+          name  = "Traces"
+          items = {this.state.traces} />
+      </Container>
+    );
+  }
+
+});
