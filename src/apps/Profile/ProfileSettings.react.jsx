c7b4d28d58a02c813ac3af4507e309c029960915
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-06-17 01:56:40 +0200

    [SYGNUI-710] Styles for profile view

--- src/apps/Profile/ProfileSettings.react.jsx
+++ src/apps/Profile/ProfileSettings.react.jsx
@@ -11,6 +11,8 @@ var React                = require('react'),
     mui                  = require('material-ui'),
     TextField            = mui.TextField,
     RaisedButton         = mui.RaisedButton,
+    Paper                = mui.Paper,
+
     Notification         = require('../../common/Notification/Notification.react');
 
 
@@ -37,34 +39,34 @@ module.exports = React.createClass({
   headerBreadcrumbs: [
     {
       route: 'dashboard',
-      label: 'Home',
+      label: 'Home'
     },
     {
       route: 'profile-settings',
-      label: 'Account',
+      label: 'Account'
     },
     {
       route: 'profile-settings',
-      label: 'Profile',
+      label: 'Profile'
     }
   ],
 
   headerMenuItems: [
     {
       route: 'profile-settings',
-      label: 'Profile',
+      label: 'Profile'
     },
     {
       route: 'profile-authentication',
-      label: 'Authentication',
+      label: 'Authentication'
     },
     {
       route: 'profile-billing',
-      label: 'Billing',
+      label: 'Billing'
     },
     {
       route: 'profile-invitations',
-      label: 'Invitations',
+      label: 'Invitations'
     }
   ],
 
@@ -98,49 +100,98 @@ module.exports = React.createClass({
     );
   },
 
+  getStyles: function() {
+    return {
+      container: {
+        margin: '96px auto',
+        width: '100%',
+        maxWidth: 800
+      },
+      header: {
+        padding: '48px 48px 0',
+        color: 'rgba(0, 0, 0, 0.87)',
+        fontSize: 20,
+        lineHeight: '24px'
+      },
+      content: {
+        padding: 48
+      },
+      form {
+        width: 416
+      },
+      updateButton: {
+        height: 36,
+        lineHeight: '36px',
+        fontWeigh: 400,
+        boxShadow: 0
+      },
+      updateButtonLabel: {
+        lineHeight: '36px',
+        fontWeight: 400,
+        paddingLeft: 30,
+        paddingRight: 30
+      }
+    }
+  },
+
   render: function () {
+    var styles = this.getStyles();
+
     return (
-      <div className="container">
-        {this.renderError()}
-        {this.renderFeedback()}
-        <form
-          onSubmit={this.handleSubmit}
-          acceptCharset="UTF-8"
-          method="post">
-          <TextField
-            ref="firstName"
-            valueLink={this.linkState('firstName')}
-            defaultValue={this.state.firstName}
-            errorText={this.getValidationMessages('firstName').join()}
-            name="firstName"
-            className="text-field"
-            autoComplete="firstName"
-            hintText="First name" />
-          <TextField
-            ref="lastName"
-            valueLink={this.linkState('lastName')}
-            defaultValue={this.state.lastName}
-            errorText={this.getValidationMessages('lastName').join()}
-            name="lastName"
-            className="text-field"
-            autoComplete="lastName"
-            hintText="Last name" />
-          <TextField
-            ref="email"
-            name="email"
-            value={this.state.email}
-            className="text-field"
-            autoComplete="email"
-            hintText="Your email"
-            disabled={true} />
-          <RaisedButton
-            type="submit"
-            label="Update"
-            style={{height: '48px'}}
-            className="raised-button"
-            secondary={true} />
-        </form>
-      </div>
+      <Paper zDepth={1} rounded={false} style={styles.container}>
+        <div style={styles.header}>
+          Profile
+        </div>
+        <div style={styles.content}>
+          {this.renderError()}
+          {this.renderFeedback()}
+          <form
+            style={styles.form}
+            onSubmit={this.handleSubmit}
+            acceptCharset="UTF-8"
+            method="post">
+            <TextField
+              ref="firstName"
+              valueLink={this.linkState('firstName')}
+              defaultValue={this.state.firstName}
+              errorText={this.getValidationMessages('firstName').join()}
+              name="firstName"
+              floatingLabelText="First name"
+              className="text-field"
+              autoComplete="firstName"
+              hintText="First name"
+              fullWidth={true} />
+            <TextField
+              ref="lastName"
+              valueLink={this.linkState('lastName')}
+              defaultValue={this.state.lastName}
+              errorText={this.getValidationMessages('lastName').join()}
+              name="lastName"
+              floatingLabelText="Last name"
+              className="text-field"
+              autoComplete="lastName"
+              hintText="Last name"
+              fullWidth={true} />
+            <TextField
+              ref="email"
+              name="email"
+              value={this.state.email}
+              floatingLabelText="Email"
+              className="text-field vm-6-b"
+              autoComplete="email"
+              hintText="Your email"
+              disabled={true}
+              fullWidth={true} />
+            <RaisedButton
+              type="submit"
+              label="Update"
+              style={styles.updateButton}
+              labelStyle={styles.updateButtonLabel}
+              className="raised-button"
+              secondary={true} />
+          </form>
+        </div>
+      </Paper>
     );
   }
 
