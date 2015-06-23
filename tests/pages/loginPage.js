72bffad22f4144e7c545e636754140572d9ca13a
Adam Wardecki <adam@Adams-MacBook-Pro.local>
2015-06-23 13:41:02 +0200

    removed unnecessary waits from login test

--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -5,7 +5,8 @@ var LOCATORS = {
   pass_input : "input[name=password]",
   pass : "chan2015",
   login_button : "button[type=submit]",
-  main_div: "div[id=app]"
+  main_div: "div[id=app]",
+  table_first_row: "div.row"
 }
 
 
@@ -25,17 +26,15 @@ module.exports = function (client) {
             return client
                 .waitForElementVisible(LOCATORS['pass_input'], 1000)
                 .setValue(LOCATORS['pass_input'], LOCATORS['pass'])
-                .pause(3000);
         },
         clickSignInButton: function () {
             return client
                 .waitForElementVisible(LOCATORS["login_button"], 1000)
                 .click(LOCATORS['login_button'])
-                .pause(5000);
         },
         verifyLoginSuccessful: function () {
             return client
-                .waitForElementVisible(LOCATORS['main_div'], 20000)
+                .waitForElementVisible(LOCATORS['table_first_row'], 10000)
                 .assert.containsText(LOCATORS['main_div'], "Instances");
         }
     };
