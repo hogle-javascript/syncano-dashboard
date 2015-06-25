64a4d1b7d4d4d9ff4d895c0a1af73866b4dbbf8b
Daniel Kopka <daniel.kopka@syncano.com>
2015-06-25 08:58:47 +0200

    [SYNGUI-926] Profile invitations

--- src/apps/Profile/ProfileActions.js
+++ src/apps/Profile/ProfileActions.js
@@ -1,30 +1,11 @@
 var Reflux     = require('reflux'),
-    Syncano    = require('../Session/Connection'),
-    Connection = Syncano.get(),
-    D          = Syncano.D;
+    Connection = require('../Session/Connection').get();
 
 
 var ProfileActions = Reflux.createActions({
-  checkItem  : {},
-  uncheckAll : {},
-  setInvitations: {},
-
   updateSettings: {
       asyncResult: true,
       asyncForm: true,
-      children: ['completed', 'failure'],
-  },
-  fetchInvitations: {
-      asyncResult: true,
-      loading: true,
-      children: ['completed', 'failure'],
-  },
-  declineInvitations: {
-      asyncResult: true,
-      children: ['completed', 'failure'],
-  },
-  acceptInvitations: {
-      asyncResult: true,
       children: ['completed', 'failure'],
   },
   changePassword: {
@@ -43,37 +24,6 @@ ProfileActions.updateSettings.listen(function (payload) {
     })
     .then(this.completed)
     .catch(this.failure);
-});
-
-ProfileActions.fetchInvitations.listen(function () {
-  console.info('ProfileActions::fetchInvitations');
-  Connection
-    .AccountInvitations
-    .list()
-    .then(this.completed)
-    .catch(this.failure);
-});
-
-ProfileActions.acceptInvitations.listen(function (items) {
-  console.info('ProfileActions::acceptInvitations');
-  var promises = items.map(function (item) {
-    return Connection.AccountInvitations.accept(item.key);
-  });
-
-  D.all(promises)
-    .success(this.completed)
-    .error(this.failure);
-});
-
-ProfileActions.declineInvitations.listen(function (items) {
-  console.info('ProfileActions::declineInvitations');
-  var promises = items.map(function (item) {
-    return Connection.AccountInvitations.remove(item.id);
-  });
-
-  D.all(promises)
-    .success(this.completed)
-    .error(this.failure);
 });
 
 ProfileActions.changePassword.listen(function (payload) {
