be48d8a41defa2fae3140b887796fdb1a6922fa7
Hubert Wesolowski <hubert.wesolowski@syncano.com>
2015-06-26 15:30:12 +0200

    predefined crontabs

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,48 +1,1 @@
-var host, api;
-host = 'https://api.syncano.rocks';
-api = host + '/v1';
-
-module.exports = {
-
-  HOST: host,
-  API: api,
-
-  // Views constrains
-  VIEW_MODES: ['cards', 'stream'],
-  SORT_MODES: [
-      'sortByName',
-      'sortByDate',
-      'sortByFullName',
-      'sortByEmail',
-      'sortByCodeBoxName',
-      'sortByWebHookName',
-      'sortByScheduleCreateDate',
-      'sortByScheduleName',
-      'sortByTriggerName',
-      'sortByTriggerCreateDate',
-      'sortByAPIKeyDescription',
-      'sortByAPIKeyCreationDate',
-      'sortByClassCreateDate',
-      'sortByClassName'
-
-  ],
-
-  VIEW_ACTIONS_MAP: {
-      sortByName: 'id',
-      sortByDate: 'data.created_at',
-      switchToListView: 'stream',
-      switchToCardView: 'cards',
-      sortByFullName: 'fullname',
-      sortByEmail: 'data.email',
-      sortByCodeBoxName: 'data.name',
-      sortByWebHookName: 'data.slug',
-      sortByScheduleCreateDate: 'data.created_at',
-      sortByScheduleName: 'data.name',
-      sortByTriggerName: 'data.name',
-      sortByTriggerCreateDate: 'data.created_at',
-      sortByAPIKeyCreationDate: 'data.created_at',
-      sortByAPIKeyDescription: 'data.description',
-      sortByClassCreateDate: 'data.created_at',
-      sortByClassName: 'data.name'
-  }
-};
+var host, api;host = 'https://api.syncano.rocks';api = host + '/v1';module.exports = {  HOST: host,  API: api,  // Views constrains  VIEW_MODES: ['cards', 'stream'],  SORT_MODES: [      'sortByName',      'sortByDate',      'sortByFullName',      'sortByEmail',      'sortByCodeBoxName',      'sortByWebHookName',      'sortByScheduleCreateDate',      'sortByScheduleName',      'sortByTriggerName',      'sortByTriggerCreateDate',      'sortByAPIKeyDescription',      'sortByAPIKeyCreationDate',      'sortByClassCreateDate',      'sortByClassName'  ],  VIEW_ACTIONS_MAP: {      sortByName: 'id',      sortByDate: 'data.created_at',      switchToListView: 'stream',      switchToCardView: 'cards',      sortByFullName: 'fullname',      sortByEmail: 'data.email',      sortByCodeBoxName: 'data.name',      sortByWebHookName: 'data.slug',      sortByScheduleCreateDate: 'data.created_at',      sortByScheduleName: 'data.name',      sortByTriggerName: 'data.name',      sortByTriggerCreateDate: 'data.created_at',      sortByAPIKeyCreationDate: 'data.created_at',      sortByAPIKeyDescription: 'data.description',      sortByClassCreateDate: 'data.created_at',      sortByClassName: 'data.name'  },  crontabs: [    {      description : "Every minute (* * * * *)",      crontab     : "* * * * *"    }, {      description : "Every 5 minutes (*/5 * * * *)",      crontab     : "*/5 * * * *"    }, {      description : "Twice an hour (0,30 * * * *)",      crontab     : "0,30 * * * *"    }, {      description : "Once an hour (0 * * * *)",      crontab     : "0 * * * *"    }, {      description : "Twice a day (0 0,12 * * *)",      crontab     : "0 0,12 * * *"    }, {      description : "Once a day (0 0 * * *)",      crontab     : "0 0 * * *"    }, {      description : "Once a week (0 0 * * 0)",      crontab     : "0 0 * * 0"    }, {      description : "1st and 15th (0 0 1,15 * *)",      crontab     : "0 0 1,15 * *"    }, {      description : "Once a month (0 0 1 * *)",      crontab     : "0 0 1 * *"    }, {      description : "Once a year (0 0 1 1 *)",      crontab     : "0 0 1 1 *"    }  ]};
