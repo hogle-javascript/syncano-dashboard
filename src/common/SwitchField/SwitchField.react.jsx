08860fef48f0bee8ad69a9ea7548ea58a4009649
Maciej Kucharz <maciek@kucharz.net>
2015-07-02 15:45:32 +0200

    Fixing style.

--- src/common/SwitchField/SwitchField.react.jsx
+++ src/common/SwitchField/SwitchField.react.jsx
@@ -1,8 +1,8 @@
-var React        = require('react');
-var classNames   = require('classnames');
+var React      = require('react'),
+    classNames = require('classnames'),
 
-//var SwitchInput = require('./SwitchInput.react');
-var Toggle      = require('material-ui/lib/toggle');
+    mui        = require('material-ui'),
+    Toggle     = mui.Toggle;
 
 require('./SwitchField.css');
 
@@ -11,18 +11,18 @@ module.exports = React.createClass({
   displayName: 'SwitchField',
 
   propTypes: {
-    handleFieldLinkClick: React.PropTypes.func.isRequired,
-    handleSwitchClick: React.PropTypes.func.isRequired,
-    toggled: React.PropTypes.bool.isRequired,
-    textEnabled: React.PropTypes.string.isRequired,
-    textDisabled: React.PropTypes.string.isRequired,
-    name: React.PropTypes.string.isRequired,
-    heading: React.PropTypes.string.isRequired,
+    handleFieldLinkClick : React.PropTypes.func.isRequired,
+    handleSwitchClick    : React.PropTypes.func.isRequired,
+    toggled              : React.PropTypes.bool.isRequired,
+    textEnabled          : React.PropTypes.string.isRequired,
+    textDisabled         : React.PropTypes.string.isRequired,
+    name                 : React.PropTypes.string.isRequired,
+    heading              : React.PropTypes.string.isRequired
   },
 
-  getInitialState: function () {
+  getInitialState: function() {
     return {
-      toggled: this.props.toggled,      
+      toggled: this.props.toggled
     };
   },
 
@@ -33,20 +33,22 @@ module.exports = React.createClass({
   handleSwitchClick: function() {
     this.props.handleSwitchClick();
     this.setState({
-      toggled: !this.state.toggled,
+      toggled: !this.state.toggled
     })
   },
 
   getText: function() {
-    var textEnabled = <div>{this.props.textEnabled} 
-                        <span 
-                          ref="changeLink"
-                          className="action-link" 
-                          onClick={this.handleFieldLinkClick}>{' Change ' + this.props.name + "."}
-                        </span>
-                      </div>;
-    var text = this.state.toggled ? textEnabled : this.props.textDisabled;
-    return text;
+    var textEnabled = (
+      <div>{this.props.textEnabled}
+        <span
+          ref       = "changeLink"
+          className = "action-link"
+          onClick   = {this.handleFieldLinkClick}>
+          {' Change ' + this.props.name + '.'}
+        </span>
+      </div>
+    );
+    return this.state.toggled ? textEnabled : this.props.textDisabled;
   },
 
   render: function() {
@@ -57,12 +59,12 @@ module.exports = React.createClass({
           <div className="switch-field-description">{this.getText()}</div>
         </div>
         <div className="switch-field-input">
-          <Toggle 
-            ref="MUIToggle"
-            onToggle={this.handleSwitchClick}
-            defaultToggled={this.props.toggled} 
-            name={this.props.name}
-             />
+          <Toggle
+            ref            = "MUIToggle"
+            onToggle       = {this.handleSwitchClick}
+            defaultToggled = {this.props.toggled}
+            name           = {this.props.name}
+          />
         </div>
       </div>
     );
