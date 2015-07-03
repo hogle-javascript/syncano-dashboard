648c5be3219ac3341f81beda0ff1411a4f0d9b1b
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-03 12:07:39 +0200

    [SYNGUI-988] missing plugin

--- webpack.config/production.js
+++ webpack.config/production.js
@@ -3,6 +3,7 @@ var webpack = require('webpack'),
 
 config.progress = false;
 config.plugins.concat(
+  new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
   new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
   new webpack.optimize.DedupePlugin(),
   new webpack.optimize.UglifyJsPlugin()
