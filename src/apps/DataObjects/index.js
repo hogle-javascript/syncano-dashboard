90daa97724268d272957a7dba6b3ace6009ba900
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-15 10:38:35 +0200

    [SYNGUI-1050] DataObjects

---
+++ src/apps/DataObjects/index.js
@@ -0,0 +1,18 @@
+import DataObjects from './DataObjects.react';
+import DataObjectsStore from './DataObjectsStore';
+import DataObjectsActions from './DataObjectsActions';
+import DataObjectDialog from './DataObjectDialog.react';
+import DataObjectDialogStore from './DataObjectDialogStore';
+import DataObjectsRenderer from './DataObjectsRenderer';
+import CheckAvatar from './CheckAvatar.react';
+import ColumnsFilterMenu from './ColumnsFilterMenu.react';
+
+DataObjects.Actions           = DataObjectsActions;
+DataObjects.Store             = DataObjectsStore;
+DataObjects.Dialog            = DataObjectDialog;
+DataObjects.DialogStore       = DataObjectDialogStore;
+DataObjects.Renderer          = DataObjectsRenderer;
+DataObjects.ColumnsFilterMenu = ColumnsFilterMenu;
+DataObjects.CheckAvatar       = CheckAvatar;
+
+export default DataObjects;
