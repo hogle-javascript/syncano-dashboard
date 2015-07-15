0b99ced43b8b0a9f9edbd19e00fba06ddebda0c7
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-15 11:37:59 +0200

    [SYNGUI-1050] Header

---
+++ src/apps/Header/index.js
@@ -0,0 +1,20 @@
+import Header from './Header.react';
+import HeaderActions from './HeaderActions';
+import HeaderInstanceMenu from './HeaderInstanceMenu.react';
+import HeaderInstancesDropdown from './HeaderInstancesDropdown.react';
+import HeaderMenu from './HeaderMenu.react';
+import HeaderMixin from './HeaderMixin';
+import HeaderNotificationsDropdown from './HeaderNotificationsDropdown.react';
+import HeaderStore from './HeaderStore';
+import InvitationsMenuItem from './InvitationsMenuItem.react';
+
+Header.Actions               = HeaderActions;
+Header.Store                 = HeaderStore;
+Header.Mixin                 = HeaderMixin;
+Header.Menu                  = HeaderMenu;
+Header.InstanceMenu          = HeaderInstanceMenu;
+Header.InstancesDropdown     = HeaderInstancesDropdown;
+Header.NotificationsDropdown = HeaderNotificationsDropdown;
+Header.InvitationsMenuItem   = InvitationsMenuItem;
+
+export default Header;
