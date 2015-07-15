a6a3efa22bcb387211d722220b417ee1e0de6d74
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-15 12:47:02 +0200

    [SYNGUI-1050] Users

---
+++ src/apps/Users/index.js
@@ -0,0 +1,27 @@
+import Users from './Users.react';
+import UsersStore from './UsersStore';
+import UsersList from './UsersList.react';
+import UsersActions from './UsersActions';
+import UserDialog from './UserDialog.react';
+import UserDialogStore from './UserDialogStore';
+
+import GroupsActions from './GroupsActions';
+import GroupsStore from './GroupsStore';
+import GroupDialog from './GroupDialog.react';
+import GroupDialogStore from './GroupDialogStore';
+import GroupsList from './GroupsList.react';
+
+Users.Actions     = UsersActions;
+Users.Store       = UsersStore;
+Users.List        = UsersList;
+Users.Dialog      = UserDialog;
+Users.DialogStore = UserDialogStore;
+Users.Groups      = {
+  Actions     : GroupsActions,
+  Store       : GroupsStore,
+  Dialog      : GroupDialog,
+  DialogStore : GroupDialogStore,
+  List        : GroupsList
+};
+
+export default Users;
