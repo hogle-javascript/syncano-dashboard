442f4ef7a3152b1baf7448e67f472b7f0d738651
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-15 12:38:24 +0200

    [SYNGUI-1050] Tasks

---
+++ src/apps/Tasks/index.js
@@ -0,0 +1,26 @@
+import ScheduleDialog from './ScheduleDialog.react';
+import ScheduleDialogStore from './ScheduleDialogStore';
+import SchedulesActions from './SchedulesActions';
+import SchedulesList from './SchedulesList.react';
+import SchedulesStore from './SchedulesStore';
+import Tasks from './Tasks.react';
+import TriggerDialog from './TriggerDialog.react';
+import TriggerDialogStore from './TriggerDialogStore';
+import TriggersActions from './TriggersActions';
+import TriggersList from './TriggersList.react';
+import TriggersStore from './TriggersStore';
+
+Tasks.ScheduleDialog      = ScheduleDialog;
+Tasks.ScheduleDialogStore = ScheduleDialogStore;
+Tasks.SchedulesActions    = SchedulesActions;
+Tasks.SchedulesList       = SchedulesList;
+Tasks.SchedulesStore      = SchedulesStore;
+
+Tasks.TriggersList        = TriggersList;
+Tasks.TriggersStore       = TriggersStore;
+Tasks.TriggersActions     = TriggersActions;
+Tasks.TriggersActions     = TriggersActions;
+Tasks.TriggerDialog       = TriggerDialog;
+Tasks.TriggerDialogStore  = TriggerDialogStore;
+
+export default Tasks;
