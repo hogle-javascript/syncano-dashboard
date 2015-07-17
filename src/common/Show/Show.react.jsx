22acbf366698c8375bb4b44cc334696e6304a55f
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-17 07:59:43 +0200

    [SYNGUI-1074] fix

--- src/common/Show/Show.react.jsx
+++ src/common/Show/Show.react.jsx
@@ -1,21 +1,10 @@
-var React         = require('react');
+var React = require('react');
 
 module.exports = React.createClass({
 
   displayName: 'Show',
 
-  getInitialState: function() {
-    return {};
-  },
-
-  componentWillReceiveProps: function(nextProps) {
-    this.setState(nextProps);
-  },
-
   render: function() {
-    if (this.state.if) {
-      return this.state.children;
-    }
-    return null;
+    return (this.props.if) ? this.props.children: null;
   }
 });
