ce59d99d00928783ca633a65a7088da1f754bf46
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-07-19 12:42:51 +0200

    [SYNGUI-1054] Addressing comments

--- src/common/Slider/Slider.react.jsx
+++ src/common/Slider/Slider.react.jsx
@@ -1,9 +1,8 @@
-var React     = require('react'),
-    Radium    = require('radium'),
+import React from 'react';
+import Radium from 'radium';
+import MUI from 'material-ui';
 
-    MUI       = require('material-ui');
-
-module.exports = Radium(React.createClass({
+export default Radium(React.createClass({
 
   displayName: 'Slider',
 
@@ -12,49 +11,51 @@ module.exports = Radium(React.createClass({
     defaultSliderValue: React.PropTypes.number
   },
 
-  getDefaultProps: function() {
+  getDefaultProps() {
     return {
       defaultSliderValue: 0
     }
   },
 
-  getInitialState: function() {
+  getInitialState() {
     return {
       selectedItem: this.props.defaultSliderValue
     }
   },
 
-  getStyles: function() {
-    var styles = {
+  getStyles() {
+    return {
       container: {
-        position: 'relative'
+        position  : 'relative'
       },
       legendItems: {
         position  : 'absolute',
         minWidth  : '100px',
         textAlign : 'center',
-        transform : 'translate(-50%, 0%)'
+        Transform : 'translateX(-50%)'
       },
       lastLegendItem: {
-        transform : 'translate(-100%, 0%) !important',
+        Transform : 'translate(-100%) !important',
         textAlign : 'right !important'
       },
       selectedItem: {
-        color : MUI.Styles.Colors.lightBlueA700
+        color     : MUI.Styles.Colors.lightBlueA700
       }
-    };
-    return styles;
+    }
   },
 
-  renderLegend: function() {
+  renderLegend() {
     var styles = this.getStyles().container;
     return (
-        <div style={styles}>{this.renderLegendItems()}</div>
+      <div style={styles}>
+        {this.renderLegendItems()}
+      </div>
     )
   },
 
-  renderLegendItems: function() {
+  renderLegendItems() {
     var styles = this.getStyles();
+
     return this.props.legendItems.map(function(item, i) {
       let position = i / (this.props.legendItems.length - 1) * 100 + '%';
       let isLastItem = i === this.props.legendItems.length - 1;
@@ -66,20 +67,21 @@ module.exports = Radium(React.createClass({
           {left: position},
           isLastItem && styles.lastLegendItem,
           isSelected && styles.selectedItem
-          ]}>
+          ]}
+        >
           {item}
         </div>
       );
     }.bind(this));
   },
 
-  setSelectedItem: function(e, value) {
+  setSelectedItem(event, value) {
     this.setState({
       selectedItem: value
     })
   },
 
-  render: function() {
+  render() {
     return (
       <div>
         <MUI.Slider
