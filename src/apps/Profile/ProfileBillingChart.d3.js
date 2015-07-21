ac487df2432bbdbffae89173762e557c9efafb17
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-21 10:24:10 +0200

    [SYNGUI-1053] wip

--- src/apps/Profile/ProfileBillingChart.d3.js
+++ src/apps/Profile/ProfileBillingChart.d3.js
@@ -1,90 +1,65 @@
 import {EventEmitter} from 'events';
 import d3 from 'd3';
+import _ from 'lodash';
 
-let data = [
-  {date: '1-May-12', close: 1},
-  {date: '2-May-12', close: 2},
-  {date: '3-May-12', close: 3},
-  {date: '4-May-12', close: 4},
-];
+import './x.css';
+
 
 class BillingChart {
 
-  constructor(element) {
-    this.element    = element;
-    this.dispatcher = new EventEmitter();
-    this.svg        = d3.select(element)
-                        .append('svg')
-                        .attr('class', 'd3');
+  constructor(element, data) {
+    let margin    = {top: 30, right: 20, bottom: 35, left: 0};
+    let width     = 700 - margin.left - margin.right;
+    let height    = 270 - margin.top - margin.bottom;
+    let parseDate = d3.time.format("%Y-%m-%d").parse;
 
-    var margin    = {top: 30, right: 20, bottom: 35, left: 50},
-        width     = 600 - margin.left - margin.right,
-        height    = 270 - margin.top - margin.bottom,
-        parseDate = d3.time.format("%d-%b-%y").parse;
+    let x = d3.time.scale().range([0, width]);
+    let y = d3.scale.linear().range([height, 0]);
 
-    var x = d3.time.scale().range([0, width]);
-    var y = d3.scale.linear().range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .ticks(5);
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
+    let xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
         .ticks(5);
 
-    var area = d3.svg.area()
-        .x(function(d) { return x(d.date); })
+    let svg = d3.select(element)
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let domain = data.domain.map(d => parseDate(d));
+    x.domain(d3.extent(domain));
+    y.domain([0, 1000]);
+
+    _.forEach(data.series, (value, key) => {
+      let area = d3.svg.area()
+        .x( d => x(parseDate(d.date)) )
         .y0(height)
-        .y1(function(d) { return y(d.close); });
+        .y1( d => y(+d.value) );
 
-    var area2 = d3.svg.area()
-        .x(function(d) { return x(d.date); })
-        .y0(height)
-        .y1(function(d) { return y(d.close+1); });
+      // Add the filled area
+      svg.append("path")
+          .datum(value)
+          .attr("class", `area area-${key}`)
+          .attr("d", area);
 
-    this.svg
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.close = +d.close;
     });
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-    // Add the filled area
-    this.svg.append("path")
-        .datum(data)
-        .attr("class", "area2")
-        .attr("d", area2);
-
-    // Add the filled area
-    this.svg.append("path")
-        .datum(data)
-        .attr("class", "area")
-        .attr("d", area);
-
     // Add the X Axis
-    this.svg.append("g")
+    svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", `translate(0, ${height})`)
         .call(xAxis);
 
     // Add the text label for the X axis
-    this.svg.append("text")
+    svg.append("text")
         .attr("transform",
               "translate(" + (width/2) + " ," +
                              (height+margin.bottom) + ")")
         .style("text-anchor", "middle")
         .text("Date");
+
   }
 
   update() {
--- src/apps/Profile/ProfileBillingChart.react.js
+++ src/apps/Profile/ProfileBillingChart.react.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import moment from 'moment';
+import _ from 'lodash';
 
 import d3Chart from './ProfileBillingChart.d3';
 
 export default React.createClass({
   chart: null,
+
+  getDates() {
+    let today        = new Date();
+    let numberOfdays = new Date(today.getFullYear(), today.getMonth() + 2, 0).getDate();
+    let days         = _.range(1, numberOfdays + 1);
+
+    return _.map(days, day => {
+      let date = new Date(today.getFullYear(), today.getMonth() + 1, day);
+      return moment(date).format('YYYY-MM-DD');
+    });
+
+  },
+
+  getChartState() {
+    let dates  = this.getDates();
+    let cb = d => {
+      return {
+        date: d,
+        value: parseInt(Math.random() * (100 - 1) + 1)
+      }
+    };
+
+    let reduceCb = (result, value, index) => {
+      if (index > 1) {
+        value.value += result[index - 2].value;
+      }
+      result.push(value);
+      return result;
+    };
+
+    return {
+      series: {
+        api: _.reduce(_.map(dates, cb), reduceCb, []),
+        cbx: _.reduce(_.map(dates, cb), reduceCb, []),
+        apiPredictions: [],
+        cbxPredictions: []
+      },
+      domain: dates
+    };
+  },
 
   getDefaultProps() {
     return {
@@ -20,7 +62,8 @@ export default React.createClass({
 
   componentDidMount() {
     let element = this.getDOMNode();
-    this.chart = new d3Chart(element);
+    let data    = this.getChartState();
+    this.chart  = new d3Chart(element, data);
   },
 
   render() {
