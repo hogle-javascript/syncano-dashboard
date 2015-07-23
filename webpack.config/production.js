565d8997c3db6b78c34fdb1e1107a9e8443e6917
Daniel Kopka <daniel.kopka@syncano.com>
2015-07-23 11:11:16 +0200

    [SYNGUI-1078] fix

--- webpack.config/production.js
+++ webpack.config/production.js
@@ -4,14 +4,19 @@ var webpack = require('webpack'),
 config.bail     = true;
 config.debug    = false;
 config.profile  = false;
-config.devtool  = 'cheap-source-map';
+config.devtool  = 'source-map';
 config.progress = false;
 config.plugins  = config.plugins.concat(
   new webpack.DefinePlugin({SYNCANO_BASE_URL: "'https://api.syncano.io/'"}),
   new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
   new webpack.optimize.OccurenceOrderPlugin(true),
   new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({ output: {comments: false} }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      drop_console: true,
+      drop_debugger: true
+    }
+  }),
   new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity})
 );
 
