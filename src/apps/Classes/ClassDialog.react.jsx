8ff497db302bd5831f87cc23a7c9c03ca2b0f35f
Adam Wardecki <adam@Adams-MacBook-Pro.local>
2015-07-28 15:33:45 +0200

    Added class selector for tests

--- src/apps/Classes/ClassDialog.react.jsx
+++ src/apps/Classes/ClassDialog.react.jsx
@@ -355,6 +355,7 @@ export default React.createClass({
             </div>
             <div className='col-xs-8' style={{paddingLeft: 15}}>
               <MUI.SelectField
+                className         = 'type-dropdown'
                 ref               = 'fieldType'
                 name              = 'fieldType'
                 floatingLabelText = 'Type'
