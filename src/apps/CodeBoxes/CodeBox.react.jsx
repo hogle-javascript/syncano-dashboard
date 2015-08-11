db4fda9972e18627240b240d66d958c64247a033
Hubert Wesolowski <hubert.wesolowski@syncano.com>
2015-08-11 23:16:00 +0200

    [SYNGUI-1213] Trigger traces

--- src/apps/CodeBoxes/CodeBox.react.jsx
+++ src/apps/CodeBoxes/CodeBox.react.jsx
@@ -150,7 +150,7 @@ export default React.createClass({
           {this.renderToolbarTitle()}
         </MUI.Toolbar>
 
-        <div style={{margin: '65px auto', width: '80%'}}>
+        <div style={{margin: '65px auto', width: '100%'}}>
 
           <div style={{paddingTop: 32}}>
             {this.renderTabs()}
--- src/apps/Traces/Traces.react.jsx
+++ src/apps/Traces/Traces.react.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 import Router from 'react-router';
+import Radium from 'radium';
 
 // Utils
 import HeaderMixin from '../Header/HeaderMixin';
@@ -18,7 +19,7 @@ import Container from '../../common/Container/Container.react';
 import TracesList from './TracesList.react';
 
 
-export default React.createClass({
+export default Radium(React.createClass({
 
   displayName: 'Traces',
 
@@ -43,6 +44,29 @@ export default React.createClass({
     }
   },
 
+  getStyles() {
+    let styles = {
+      toolbar: {
+        position: 'fixed',
+        top: 64,
+        right: 0,
+        zIndex: 8,
+        paddingLeft: 256,
+        background: 'rgba(215,215,215,0.6)',
+        padding: '0px 32px 0 24px'
+      },
+      list: {
+        position: 'relative',
+        top: '35px'
+      },
+      cBList: {
+       top: '-45px'
+      }
+    };
+
+    return styles;
+  },
+
   getConfig() {
     return {
       webhook: {
@@ -54,8 +78,8 @@ export default React.createClass({
         backLabel: 'Go back to CodeBoxes list'
       },
       trigger: {
-        route: 'data',
-        backLabel: 'Go back to Data Views'
+        route: 'tasks',
+        backLabel: 'Go back to Tasks list'
       },
       schedule: {
         route: 'tasks',
@@ -86,17 +110,11 @@ export default React.createClass({
 
   render() {
     const config = this.getConfig();
+    const styles = this.getStyles();
 
     return (
       <div>
-        <MUI.Toolbar style={{
-          position: 'fixed',
-          top: 64,
-          right: 0,
-          zIndex: 8,
-          paddingLeft: 256,
-          background: 'rgba(215,215,215,0.6)',
-          padding: '0px 32px 0 24px'}}>
+        <MUI.Toolbar style={styles.toolbar}>
           <MUI.ToolbarGroup>
             <MUI.IconButton
               iconClassName="synicon-arrow-left"
@@ -110,11 +128,13 @@ export default React.createClass({
 
           {this.renderToolbarTitle()}
         </MUI.Toolbar>
-        <TracesList
-          tracesFor={this.props.tracesFor}
-          name="Traces"
-          items={this.state.traces}/>
+        <div style={[styles.list, this.isActive('codebox-traces') && styles.cBList]}>
+          <TracesList
+            tracesFor={this.props.tracesFor}
+            name="Traces"
+            items={this.state.traces}/>
+        </div>
       </div>
     );
   }
-});
+}));
