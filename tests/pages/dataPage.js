08c9574ca60716efe710dc7425240c8c33c70c7c
patrykkopycinski <contact@patrykkopycinski.com>
2015-08-27 11:32:19 +0200

    Revert "[SYNGUI-1508] Remove (for now) Data Endpoints"

--- tests/pages/dataPage.js
+++ tests/pages/dataPage.js
@@ -18,10 +18,6 @@ module.exports = {
       selector: '//div[text()="webhook_description"]',
       locateStrategy: 'xpath'
     },
-    classListItem: {
-      selector: '//div[text()="user_profile"]',
-      locateStrategy: 'xpath'
-    },
     dataListItem: {
       selector: '//div[text()="data_view"]',
       locateStrategy: 'xpath'
