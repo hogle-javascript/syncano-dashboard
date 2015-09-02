58a259c12b09eac9999de1346e4fdc8a482573d6
Adam Wardecki <adam@Adams-MacBook-Pro.local>
2015-09-02 18:19:00 +0200

    [TNB-120] Basic Solutions Tests

--- tests/pages/solutionsPage.js
+++ tests/pages/solutionsPage.js
@@ -1,11 +1,12 @@
-var solutionsCommands = {
+const solutionsCommands = {
   clickButton: function(button) {
-    return this.waitForElementVisible(button, 5000)
+    return this.waitForElementVisible(button)
       .click(button);
-  },
+  }
 };
 
 module.exports = {
+  url: 'https://localhost:8080/#/solutions/list',
   commands: [solutionsCommands],
   elements: {
     solutionsView: {
@@ -13,6 +14,42 @@ module.exports = {
     },
     solutionDetails: {
       selector: '//button//span[text()="SEE DETAILS"]',
+      locateStrategy: 'xpath'
+    },
+    favorite: {
+      selector: '//div[@class="vm-6-b"]//div[text()="Favorite"]',
+      locateStrategy: 'xpath'
+    },
+    mySolutions: {
+      selector: '//div[@class="vm-6-b"]//div[text()="My solutions"]',
+      locateStrategy: 'xpath'
+    },
+    favoriteSolutionTitle: {
+      selector: '//div[@title="SlackNotifications"]',
+      locateStrategy: 'xpath'
+    },
+    mySolutionTitle: {
+      selector: '//div[@title="my solution"]',
+      locateStrategy: 'xpath'
+    },
+    tagsList: {
+      selector: '//div[@class="tags-list"]',
+      locateStrategy: 'xpath'
+    },
+    tagsListItem: {
+      selector: '//div[@class="tags-list"]//div[text()="js"]',
+      locateStrategy: 'xpath'
+    },
+    tagListItemCount: {
+      selector: '//div[@class="tags-list"]//div[text()="js"]/preceding-sibling::div',
+      locateStrategy: 'xpath'
+    },
+    tagsListJs: {
+      selector: '//div[@class="tags-list"]//div[text()="js"]',
+      locateStrategy: 'xpath'
+    },
+    tagsJs: {
+      selector: '//span[@class="synicon-tag"]/following-sibling::a[text()="js"]',
       locateStrategy: 'xpath'
     }
   }
--- tests/solutions/testSolutions.js
+++ tests/solutions/testSolutions.js
@@ -1,4 +1,3 @@
-const utils = require('../utils');
 
 module.exports = {
   tags: ['solutions'],
@@ -18,15 +17,34 @@ module.exports = {
     const solutionsPage = client.page.solutionsPage();
 
     solutionsPage.navigate();
+    solutionsPage.clickButton('@favorite');
+    solutionsPage.waitForElementVisible('@favoriteSolutionTitle');
   },
   'Administrator can view his Solutions' : function(client) {
     const solutionsPage = client.page.solutionsPage();
 
     solutionsPage.navigate();
+    solutionsPage.clickButton('@mySolutions');
+    solutionsPage.waitForElementVisible('@mySolutionTitle');
   },
   'Administrator can filter solutions by tags' : function(client) {
+    var tagsCount = null;
     const solutionsPage = client.page.solutionsPage();
+    const elementsWithTag = solutionsPage.elements.tagsJs;
 
     solutionsPage.navigate();
+    solutionsPage.waitForElementVisible('@tagsList');
+    solutionsPage.clickButton('@tagsListJs');
+    solutionsPage.waitForElementVisible('@tagsJs');
+
+    solutionsPage.getText('@tagListItemCount', function(result) {
+      tagsCount = parseInt(result.value, 10);
+    });
+
+    client.elements(elementsWithTag.locateStrategy, elementsWithTag.selector, function(result) {
+      if (tagsCount >= result.value.length) {
+        client.assert.ok(true, 'Tags count is equal or greater than number of solutions on the list');
+      }
+    });
   }
 };
