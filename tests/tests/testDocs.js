25cf395d6e590f365665ecdf96463abd22f2bdb0
Adam Wardecki <adam.wardecki@gmail.com>
2015-09-29 16:43:51 +0200

    [TNB-162] Changing vars to consts

--- tests/tests/testDocs.js
+++ tests/tests/testDocs.js
@@ -1,7 +1,8 @@
 module.exports = {
   tags: ['docs'],
   before: function(client) {
-    var loginPage = client.page.loginPage();
+    const loginPage = client.page.loginPage();
+
     loginPage.navigate();
     loginPage.typeEmail();
     loginPage.typePassword();
@@ -12,14 +13,14 @@ module.exports = {
     client.end();
   },
   'User Goes to Docs page': function(client) {
-    var instancesPage = client.page.instancesPage();
-    var docsPage = client.page.docsPage();
+    const instancesPage = client.page.instancesPage();
+    const docsPage = client.page.docsPage();
 
     instancesPage.clickButton('@docsLink')
     client.pause(1000);
 
     client.windowHandles(function(result) {
-      var handle = result.value[1];
+      const handle = result.value[1];
       client.switchWindow(handle);
     });
 
