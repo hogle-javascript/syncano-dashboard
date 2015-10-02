8cb9e1d13710e237d9ecb5615d16197020fbb172
Daniel Kopka <daniel.kopka@syncano.com>
2015-10-02 15:16:27 +0200

    [SYNGUI-1622] Feature names in changelog

--- gulp-tasks/changelog.js
+++ gulp-tasks/changelog.js
@@ -1,8 +1,21 @@
 var async   = require('async'),
+    gutil   = require('gulp-util'),
     git     = require('gulp-git'),
-    _       = require('lodash');
+    _       = require('lodash'),
+    request = require('superagent');
 
 module.exports = function(cb) {
+  var token = process.env.AHA_TOKEN;
+  var account = process.env.AHA_ACCOUNT;
+
+  if (!token) {
+    throw new gutil.PluginError('changelog', '"AHA_TOKEN" env variable is required');
+  }
+
+  if (!account) {
+    throw new gutil.PluginError('changelog', '"AHA_ACCOUNT" env variable is required');
+  }
+
   async.waterfall([
     function (callback) {
       // Fetch tags from origin
@@ -19,6 +32,7 @@ module.exports = function(cb) {
     },
 
     function(start, end, callback) {
+      // Parse git log in search of ticket numbers
       var range = start + '...' + end;
       var command = 'log ' + range + ' --grep="[[:alpha:]{5, 10}][-][[:digit:]]" --oneline --pretty=format:"%s"';
       var regex = /[A-Za-z]{5,10}-[\d]+/gm;
@@ -26,20 +40,52 @@ module.exports = function(cb) {
         if (err) return callback(err);
         callback(null, _.uniq(stdout.match(regex)), end);
       });
+    },
+
+    function(tickets, tag, callback) {
+      // Fetch tickets details from AHA
+      async.mapLimit(tickets, 3, function(ticket, iteratorCallback) {
+        gutil.log('[changelog]', 'Fetching details for ' + ticket);
+        request
+          .get('https://' + account + '.aha.io/api/v1/features/' + ticket)
+          .set('Authorization', 'Basic ' + token)
+          .end(function(err, response) {
+            if (err || response.statusCode !== 200) {
+              return iteratorCallback(err, {
+                name: '',
+                type: 'Other',
+                resource: '',
+                reference_num: ticket
+              })
+            }
+
+            var feature = response.body.feature;
+            iteratorCallback(null, {
+              name: feature.name,
+              type: feature.workflow_kind.name,
+              reference_num: feature.reference_num,
+              resource: feature.resource
+            })
+          });
+      }, function(err, results) {
+        callback(null, results, tag);
+      });
     }
   ], function(err, tickets, tag) {
     if (err) throw err;
 
+    var groupedTickets = _.reduce(tickets, function(result, value) {
+      result[value.type] = result[value.type] || [];
+      result[value.type].push('[' + value.reference_num + '] ' + value.name);
+      return result;
+    }, {});
+
     console.log('\n\nChangelog for version:', tag + ':\n');
 
-    tickets.sort();
-    _.forEach(tickets, function(ticket) {
-      if (_.startsWith(ticket, 'SYN')) {
-        console.log(_.padRight(ticket, 15), 'https://syncano.aha.io/features/' + ticket);
-      } else {
-        console.log(ticket);
-      }
+    _.forEach(groupedTickets, function(changelog, type) {
+      console.log(type + ':\n' + changelog.join('\n') + '\n\n');
     });
+
     cb();
   });
 };
