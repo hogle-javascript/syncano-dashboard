42ebec2970c0a2802aac131d06a71e92eb48ea67
Adam Wardecki <adam.wardecki@gmail.com>
2015-10-09 18:09:44 +0200

    [TNB-178] Admin can't delete user_profile class

--- tests/instances/testClasses.js
+++ tests/instances/testClasses.js
@@ -2,7 +2,7 @@ const utils = require('../utils');
 
 module.exports = {
   tags: ['classes'],
-  before: function(client) {
+  before(client) {
     const loginPage = client.page.loginPage();
 
     loginPage.navigate();
@@ -11,15 +11,15 @@ module.exports = {
     loginPage.clickSignInButton();
     loginPage.verifyLoginSuccessful();
   },
-  after: function(client) {
+  after(client) {
     client.end();
   },
-  'Test Add Class' : function(client) {
+  'Test Add Class': function addClass(client) {
     const className = utils.addSuffix('class');
     const classesPage = client.page.classesPage();
 
     classesPage.navigate();
-    classesPage.clickFAB();
+    classesPage.clickButton('@fab');
     classesPage.fillInputField('@createModalNameInput', className);
     classesPage.fillInputField('@createModalDescriptionInput', 'nightwatch_test_class_description');
     classesPage.fillInputField('@createModalFieldNameInput', 'schemaName');
@@ -33,26 +33,33 @@ module.exports = {
     classesPage.waitForElementVisible('@classTableRow');
     classesPage.verify.containsText('@classTableRowDescription', 'nightwatch_test_class_description');
   },
-  'Test Edit Class' : function(client) {
+  'Test Edit Class': function editClass(client) {
     const classesPage = client.page.classesPage();
 
-    classesPage.clickSelectClass();
+    classesPage.clickButton('@selectClass');
     classesPage.clickButton('@editButton');
-    classesPage.fillClassDescription('nightwatch_test_class_new_description');
+    classesPage.fillInputField('@createModalDescriptionInput', 'nightwatch_test_class_new_description');
     classesPage.clickButton('@confirmButton');
     classesPage.waitForElementNotVisible('@editClassTitle');
     client.pause(1000);
     classesPage.waitForElementVisible('@classTableRowDescription');
     classesPage.verify.containsText('@classTableRowDescription', 'nightwatch_test_class_new_description');
   },
-  'Test Delete Class' : function(client) {
+  'Test Delete Class': function deleteClass(client) {
     const classesPage = client.page.classesPage();
 
-    classesPage.clickSelectClass();
+    classesPage.clickButton('@selectClass');
     classesPage.clickButton('@deleteButton');
     client.pause(1000);
     classesPage.clickButton('@confirmDeleteButton');
     classesPage.waitForElementNotVisible('@deleteClassModalTitle');
     classesPage.waitForElementNotPresent('@classTableName');
+  },
+  'Test Admin cannot delete user_profile class': function cantDeleteUserClass(client) {
+    const classesPage = client.page.classesPage();
+
+    classesPage.navigate();
+    classesPage.clickButton('@selectUserClass');
+    classesPage.waitForElementVisible('@inactiveDeleteButton');
   }
 };
