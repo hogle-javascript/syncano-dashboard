e7709eb12c5b44320bb2a3609c1aaf3660df406b
Daniel Kopka <daniel.kopka@syncano.com>
2015-10-12 08:14:53 +0200

    [TNB-175] bash cleanup

--- test.sh
+++ test.sh
@@ -12,28 +12,36 @@ function run_e2e_tests {
     npm run-script e2e-setup
     nohup npm run-script e2e-http-server &
     sleep 5
-    npm run-script e2e-screenshots
+    npm run-script e2e
     rm -rf ./dist_e2e
 }
 
-if [[ "$CIRCLE_BRANCH" == "screenshots" && "$CIRCLE_NODE_INDEX" == 1 ]]; then
+function run_e2e_screenshots {
     git fetch --all
     git reset --hard origin/TNB-175
-fi
+    npm run-script build
+    mv ./dist ./dist_e2e
+    npm run-script e2e-setup
+    nohup npm run-script e2e-http-server &
+    sleep 5
+    npm run-script e2e-screenshots
+    rm -rf ./dist_e2e
+    npm run-script upload-screenshots
+}
 
 case "$CIRCLE_NODE_INDEX" in
     0)
         run_unit_tests
         ;;
     1)
-        run_e2e_tests
+        if [[ "$CIRCLE_BRANCH" == "screenshots" ]]; then
+            run_e2e_screenshots
+        else
+            run_e2e_tests
+        fi
         ;;
     *)
         run_unit_tests
         run_e2e_tests
         ;;
 esac
-
-if [[ "$CIRCLE_BRANCH" == "screenshots" && "$CIRCLE_NODE_INDEX" == 1 ]]; then
-    npm run-script upload-screenshots
-fi
