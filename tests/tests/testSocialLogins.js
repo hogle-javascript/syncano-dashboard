f8076292dcfaa1b83131b8ef6762bff7f1df8f54
Adam Wardecki <adam.wardecki@gmail.com>
2015-10-15 16:26:26 +0200

    [TNB-188] Check if google test works on CI

--- tests/tests/testSocialLogins.js
+++ tests/tests/testSocialLogins.js
@@ -8,36 +8,31 @@ module.exports = {
   afterEach(client, done) {
     client.end(done);
   },
-  // This test works locally but will have to wait for gmail
-  // Activity alerts to be turned off. Tests run from circle
-  // trigger extra verification steps triggered. It takes a week to be turned off
-  // https://support.google.com/mail/answer/45938?hl=en
+  'Admin Logs in with Google': function GoogleLogin(client) {
+    const loginPage = client.page.loginPage();
+    const instancesPage = client.page.instancesPage();
 
-  // 'Admin Logs in with Google': function GoogleLogin(client) {
-  //   const loginPage = client.page.loginPage();
-  //   const instancesPage = client.page.instancesPage();
+    loginPage.clickButton('@loginButtonGoogle')
+    client.pause(1000);
+    client.windowHandles(function(result) {
+      const handle = result.value[1];
 
-  //   loginPage.clickButton('@loginButtonGoogle')
-  //   client.pause(1000);
-  //   client.windowHandles(function(result) {
-  //     const handle = result.value[1];
+      client.switchWindow(handle);
+    });
+    loginPage.fillInputField('@emailInputGoogle', process.env.NIGHTWATCH_EMAIL)
+      .clickButton('@nextButtonGoogle')
+      .fillInputField('@passInputGoogle', process.env.NIGHTWATCH_PASSWORD)
+      .clickButton('@signInButtonGoogle')
+    client.pause(2000);
+    loginPage.clickButton('@approveAccessButtonGoogle')
 
-  //     client.switchWindow(handle);
-  //   });
-  //   loginPage.fillInputField('@emailInputGoogle', process.env.NIGHTWATCH_EMAIL)
-  //     .clickButton('@nextButtonGoogle')
-  //     .fillInputField('@passInputGoogle', process.env.NIGHTWATCH_PASSWORD)
-  //     .clickButton('@signInButtonGoogle')
-  //   client.pause(2000);
-  //   loginPage.clickButton('@approveAccessButtonGoogle')
+    client.windowHandles(function(result) {
+      const handle = result.value[0];
 
-  //   client.windowHandles(function(result) {
-  //     const handle = result.value[0];
-
-  //     client.switchWindow(handle);
-  //   });
-  //   instancesPage.waitForElementPresent('@instancesTable')
-  // },
+      client.switchWindow(handle);
+    });
+    instancesPage.waitForElementPresent('@instancesTable')
+  },
   'Admin Logs in with Facebook': function FacebookLogin(client) {
     const loginPage = client.page.loginPage();
     const instancesPage = client.page.instancesPage();
