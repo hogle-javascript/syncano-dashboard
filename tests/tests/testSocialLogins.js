b24142508b653bd1b7ae6d7ecff669db76a2e94e
Adam Wardecki <adam.wardecki@gmail.com>
2015-10-16 09:32:42 +0200

    [TNB-186] Facebook login test

--- tests/tests/testSocialLogins.js
+++ tests/tests/testSocialLogins.js
@@ -8,31 +8,6 @@ module.exports = {
   afterEach(client, done) {
     client.end(done);
   },
-  'Admin Logs in with Google': function GoogleLogin(client) {
-    const loginPage = client.page.loginPage();
-    const instancesPage = client.page.instancesPage();
-
-    loginPage.clickButton('@loginButtonGoogle')
-    client.pause(1000);
-    client.windowHandles(function(result) {
-      const handle = result.value[1];
-
-      client.switchWindow(handle);
-    });
-    loginPage.fillInputField('@emailInputGoogle', process.env.NIGHTWATCH_EMAIL)
-      .clickButton('@nextButtonGoogle')
-      .fillInputField('@passInputGoogle', process.env.NIGHTWATCH_PASSWORD)
-      .clickButton('@signInButtonGoogle')
-    client.pause(2000);
-    loginPage.clickButton('@approveAccessButtonGoogle')
-
-    client.windowHandles(function(result) {
-      const handle = result.value[0];
-
-      client.switchWindow(handle);
-    });
-    instancesPage.waitForElementPresent('@instancesTable')
-  },
   'Admin Logs in with Facebook': function FacebookLogin(client) {
     const loginPage = client.page.loginPage();
     const instancesPage = client.page.instancesPage();
@@ -55,27 +30,4 @@ module.exports = {
     });
     instancesPage.waitForElementPresent('@instancesTable')
   }
-  // 'Admin Logs in with Github': function GithubLogin(client) {
-  //   const loginPage = client.page.loginPage();
-  //   const instancesPage = client.page.instancesPage();
-
-
-  //   loginPage.clickButton('@loginButtonGithub')
-  //   client.pause(1000);
-  //   client.windowHandles(function(result) {
-  //     const handle = result.value[1];
-
-  //     client.switchWindow(handle);
-  //   });
-  //   loginPage.fillInputField('@emailInputGithub', process.env.NIGHTWATCH_EMAIL)
-  //     .fillInputField('@passInputGithub', process.env.NIGHTWATCH_PASSWORD)
-  //     .clickButton('@signInButtonGithub');
-
-  //   client.windowHandles(function(result) {
-  //     const handle = result.value[0];
-
-  //     client.switchWindow(handle);
-  //   });
-  //   instancesPage.waitForElementPresent('@instancesTable')
-  // }
 };
