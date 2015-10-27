e0620753d568e58b7c388bbd548a7457a9374e32
Adam Wardecki <adam.wardecki@gmail.com>
2015-10-27 12:46:36 +0100

    [FRONT-613] Admin selects/deletes multiple webhooks

---
+++ tests/instances/testWebhook.js
@@ -0,0 +1,57 @@
+const utils = require('../utils');
+
+module.exports = {
+  tags: ['webhook'],
+  before: function(client) {
+    const loginPage = client.page.loginPage();
+
+    loginPage.navigate();
+    loginPage.typeEmail();
+    loginPage.typePassword();
+    loginPage.clickSignInButton();
+    loginPage.verifyLoginSuccessful();
+
+  },
+  after: function(client) {
+    client.end();
+  },
+  'User adds a Webhook' : function(client) {
+    const webhook = utils.addSuffix('webhook');
+    const dataPage = client.page.dataPage();
+
+    dataPage.navigate();
+    dataPage.waitForElementVisible('@webhookListItem');
+    dataPage.clickButton('@addWebhookButton');
+    dataPage.waitForElementVisible('@addWebhookModalTitle');
+    dataPage.fillInputField('@addWebhookModalNameInput', webhook);
+    dataPage.selectFromDropdown('@addWebhookModalCodeboxDropdown', '@addWebhookModalCodeboxDropdownChoice');
+    dataPage.clickButton('@confirmButton');
+    dataPage.waitForElementVisible('@webhookTableRow');
+  },
+  'User edits a Webhook' : function(client) {
+    const dataPage = client.page.dataPage();
+
+    dataPage.navigate();
+    dataPage.waitForElementVisible('@webhookListItem');
+    dataPage.clickWebhookDropdown();
+    dataPage.clickButton('@editDropdownItem');
+    dataPage.waitForElementVisible('@editWebhookModalTitle');
+    dataPage.fillInputField('@addWebhookModalDescriptionInput', 'webhook_description');
+    client.pause(1000);
+    dataPage.clickButton('@confirmButton');
+    dataPage.waitForElementVisible('@webhookTableRow');
+    dataPage.waitForElementVisible('@webhookTableRowDescription')
+  },
+  'User deletes a Webhook' : function(client) {
+    const dataPage = client.page.dataPage();
+
+    dataPage.navigate();
+    dataPage.waitForElementVisible('@webhookListItem');
+    dataPage.clickButton('@selectWebhookTableRow');
+    dataPage.clickButton('@deleteButton');
+    dataPage.waitForElementVisible('@deleteWebhookModalTitle');
+    client.pause(1000);
+    dataPage.clickButton('@confirmButton');
+    dataPage.waitForElementNotPresent('@selectWebhookTableRow');
+  }
+};
