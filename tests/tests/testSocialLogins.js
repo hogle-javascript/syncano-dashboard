3e6b316499b1b5e0d5adf98085690c33dcf5135b
Adam Wardecki <adam.wardecki@syncano.com>
2015-10-27 19:18:21 +0100

    [FRONT-482] Re-adding google social login test

--- tests/tests/testSocialLogins.js
+++ tests/tests/testSocialLogins.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   tags: ['socialLogins'],
   beforeEach(client) {
     const loginPage = client.page.loginPage();
@@ -13,13 +13,15 @@ module.exports = {
     const instancesPage = client.page.instancesPage();
 
     loginPage.clickButton('@loginButtonFacebook');
-    client.pause(1000);
-    client.windowHandles(function(result) {
-      const handle = result.value[1];
+    client
+      .pause(1000)
+      .windowHandles(function(result) {
+        const handle = result.value[1];
 
-      client.switchWindow(handle);
-    });
-    loginPage.fillInputField('@emailInputFacebook', process.env.NIGHTWATCH_EMAIL)
+        client.switchWindow(handle);
+      });
+    loginPage
+      .fillInputField('@emailInputFacebook', process.env.NIGHTWATCH_EMAIL)
       .fillInputField('@passInputFacebook', process.env.NIGHTWATCH_PASSWORD)
       .clickButton('@signInButtonFacebook');
 
@@ -35,20 +37,23 @@ module.exports = {
     const instancesPage = client.page.instancesPage();
 
     loginPage.clickButton('@loginButtonGoogle');
-    client.pause(1000);
-    client.windowHandles(function(result) {
-      const handle = result.value[1];
+    client
+      .pause(1000)
+      .windowHandles((result) => {
+        const handle = result.value[1];
 
-      client.switchWindow(handle);
-    });
-    loginPage.fillInputField('@emailInputGoogle', process.env.NIGHTWATCH_EMAIL)
+        client.switchWindow(handle);
+      });
+    loginPage
+      .fillInputField('@emailInputGoogle', process.env.NIGHTWATCH_EMAIL)
       .clickButton('@nextButtonGoogle')
       .fillInputField('@passInputGoogle', process.env.NIGHTWATCH_PASSWORD)
       .clickButton('@signInButtonGoogle');
+
     client.pause(2000);
     loginPage.clickButton('@approveAccessButtonGoogle');
 
-    client.windowHandles(function(result) {
+    client.windowHandles((result) => {
       const handle = result.value[0];
 
       client.switchWindow(handle);
