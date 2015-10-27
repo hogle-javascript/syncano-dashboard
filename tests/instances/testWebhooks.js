fe234b9c19fcd81127999f022d0b2dc8ed7665e8
Adam Wardecki <adam.wardecki@syncano.com>
2015-10-27 18:54:20 +0100

    [FRONT-613] Addressing comments

--- tests/instances/testWebhooks.js
+++ tests/instances/testWebhooks.js
@@ -1,4 +1,3 @@
-import utils from '../utils';
 import globals from '../globals';
 import Syncano from 'syncano';
 
@@ -8,22 +7,21 @@ export default {
     const syncano = new Syncano({accountKey: globals.tempAccountKey, baseUrl: 'https://api.syncano.rocks'});
     const loginPage = client.page.loginPage();
 
-    const cb_options = {
-      'label': 'codebox',
-      'source': 'print "foo"',
-      'runtime_name': 'python'
-    }
-    const webhook_options = {
-      'name': null,
-      'codebox': null,
-    }
-    let i = 0;
+    const codeBoxOptions = {
+      label: 'codebox',
+      source: 'print "foo"',
+      runtime_name: 'python'
+    };
+    const webhookOptions = {
+      name: null,
+      codebox: null
+    };
 
-    syncano.instance(globals.tempInstanceName).codebox().add(cb_options).then((success) => {
-      webhook_options.codebox = success.id;
-      for (i; i < 3; i += 1) {
-        webhook_options.name = `webhook_${i.toString()}`;
-        syncano.instance(globals.tempInstanceName).webhook().add(webhook_options);
+    syncano.instance(globals.tempInstanceName).codebox().add(codeBoxOptions).then((success) => {
+      webhookOptions.codebox = success.id;
+      for (let i = 0; i < 3; i += 1) {
+        webhookOptions.name = `webhook_${i.toString()}`;
+        syncano.instance(globals.tempInstanceName).webhook().add(webhookOptions);
       }
     });
 
@@ -55,9 +53,9 @@ export default {
 
     client.elements('css selector', dataPage.elements.webhookToSelect.selector, (result) => {
       client.assert.equal(result.value.length, 3);
-    });  
+    });
   },
-    'Test Delete multiple Webhooks': (client) => {
+  'Test Delete multiple Webhooks': (client) => {
     const dataPage = client.page.dataPage();
 
     client.url(`https://localhost:8080/#/instances/${globals.tempInstanceName}/webhooks`);
