169062cb3c38e758c52ece69347f0f8d13d221fb
Maciej Kucharz <maciek@kucharz.net>
2015-10-30 10:53:18 +0100

    [FRONT-636] It is possible to copy channel URL now (from channels list)

--- src/apps/Channels/ChannelsList.react.jsx
+++ src/apps/Channels/ChannelsList.react.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 import Router from 'react-router';
+import ReactZeroClipboard from 'react-zeroclipboard';
 
 // Utils
 import HeaderMixin from '../Header/HeaderMixin';
@@ -11,6 +12,7 @@ import Actions from './ChannelsActions';
 import Store from './ChannelsStore';
 
 // Components
+import MUI from 'material-ui';
 import Common from '../../common';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 
@@ -38,19 +40,53 @@ export default React.createClass({
     Actions.checkItem(id, state);
   },
 
+  handleURLClick(event) {
+    event.stopPropagation();
+    this.refs.snackbar.show();
+    setTimeout(() => {
+      this.refs.snackbar.dismiss();
+    }, 1200);
+  },
+
+  renderCopyLinkIcon(item) {
+    let webhookLink = SYNCANO_BASE_URL.slice(0, -1) + item.links.poll;
+
+    return (
+      <div>
+        <ReactZeroClipboard text={webhookLink}>
+          <MUI.IconButton
+            style={{height: 20, width: 20, padding: '0 50', cursor: 'copy'}}
+            iconStyle={{fontSize: 15, color: '#9B9B9B'}}
+            iconClassName="synicon-link-variant"
+            tooltip="Copy Webhook URL"
+            onClick={this.handleURLClick}/>
+        </ReactZeroClipboard>
+      </div>
+    );
+  },
+
   renderItem(item) {
+    let copyLinkIcon = this.renderCopyLinkIcon(item);
+
     return (
       <Common.ColumnList.Item
         checked={item.checked}
         key={item.id}>
         <Column.CheckIcon
+          className="col-xs-12"
           id = {item.name}
           icon = {'bullhorn'}
           background = {Common.Color.getColorByName('blue', 'xlight')}
           checked = {item.checked}
           handleIconClick = {this.handleItemIconClick}
           handleNameClick = {this.handleItemClick} >
-          {item.name}
+          <div onClick={this.handleURLClick}>{item.name}</div>
+          <div style={{display: 'flex', flexWrap: 'wrap', cursor: 'copy'}}>
+            <div style={{fontSize: '0.8em', color: '#9B9B9B'}} onClick={this.handleURLClick}>
+              {item.links.poll}
+            </div>
+            <div>{copyLinkIcon}</div>
+          </div>
         </Column.CheckIcon>
         <Column.Desc>{item.description}</Column.Desc>
         <Column.Desc className="col-xs-5 col-md-5">
@@ -99,6 +135,7 @@ export default React.createClass({
         <Column.MenuDialog ref="menuDialog"/>
         <Common.ColumnList.Header>
           <Column.ColumnHeader
+            className="col-xs-12"
             primary={true}
             columnName="CHECK_ICON">
             {this.props.name}
@@ -127,6 +164,9 @@ export default React.createClass({
             {this.renderList()}
           </Common.Loading>
         </Common.Lists.List>
+        <MUI.Snackbar
+          ref="snackbar"
+          message="URL copied to the clipboard"/>
       </Common.Lists.Container>
     );
   }
