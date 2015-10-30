5b77c8df9e79494edb04183e544e5fb9311afcba
Maciej Kucharz <maciek@kucharz.net>
2015-10-30 11:05:00 +0100

    [FRONT-636] Moving webhooks

--- src/apps/Data/index.js
+++ src/apps/Data/index.js
@@ -6,24 +6,10 @@ import DataViewsActions from './DataViewsActions';
 import DataViewDialog from './DataViewDialog.react';
 import DataViewDialogStore from './DataViewDialogStore';
 
-import WebhooksStore from './WebhooksStore';
-import WebhooksList from './WebhooksList.react';
-import WebhooksActions from './WebhooksActions';
-import WebhookDialog from './WebhookDialog.react';
-import WebhookDialogStore from './WebhookDialogStore';
-import WebhookTraces from './WebhookTraces.react';
-
 Data.Actions = DataViewsActions;
 Data.Store = DataViewsStore;
 Data.List = DataViewsList;
 Data.Dialog = DataViewDialog;
 Data.DialogStore = DataViewDialogStore;
-
-Data.WebhooksActions = WebhooksActions;
-Data.WebhooksStore = WebhooksStore;
-Data.WebhooksList = WebhooksList;
-Data.WebhookDialog = WebhookDialog;
-Data.WebhookDialogStore = WebhookDialogStore;
-Data.WebhookTraces = WebhookTraces;
 
 export default Data;
