6c45321351a69dde08cada9c82ad9811c07f71e5
Adam Wardecki <adam.wardecki@gmail.com>
2015-11-03 11:22:31 +0100

    [FRONT-737] Fix e2e tests at sockets branch

---
+++ tests/instances/testCodeBoxSockets.js
@@ -0,0 +1,62 @@
+import utils from '../utils';
+
+export default {
+  tags: ['codeBoxSockets'],
+  before(client) {
+    const loginPage = client.page.loginPage();
+
+    loginPage
+      .navigate()
+      .typeEmail()
+      .typePassword()
+      .clickSignInButton()
+      .verifyLoginSuccessful();
+  },
+  after(client) {
+    client.end();
+  },
+  'User adds a Webhook': (client) => {
+    const webhook = utils.addSuffix('webhook');
+    const socketsPage = client.page.socketsPage();
+
+    socketsPage
+      .navigate()
+      .waitForElementVisible('@webhookListItem')
+      .clickButton('@addWebhookButton')
+      .waitForElementVisible('@addWebhookModalTitle')
+      .fillInputField('@addWebhookModalNameInput', webhook)
+      .selectFromDropdown('@addWebhookModalCodeboxDropdown', '@addWebhookModalCodeboxDropdownChoice')
+      .clickButton('@confirmButton')
+      .waitForElementVisible('@webhookTableRow');
+  },
+  // 'User edits a Webhook': (client) => {
+  //   const socketsPage = client.page.socketsPage();
+
+  //   socketsPage
+  //     .navigate()
+  //     .waitForElementVisible('@webhookListItem')
+  //     .clickWebhookDropdown()
+  //     .clickButton('@editDropdownItem')
+  //     .waitForElementVisible('@editWebhookModalTitle')
+  //     .fillInputField('@addWebhookModalDescriptionInput', 'webhook_description');
+  //   client.pause(1000);
+  //   socketsPage
+  //     .clickButton('@confirmButton')
+  //     .waitForElementVisible('@webhookTableRow')
+  //     .waitForElementVisible('@webhookTableRowDescription');
+  // },
+  'User deletes a Webhook': (client) => {
+    const socketsPage = client.page.socketsPage();
+
+    socketsPage
+     .navigate()
+     .waitForElementVisible('@webhookListItem')
+     .clickButton('@selectWebhookTableRow')
+     .clickButton('@deleteButton')
+     .waitForElementVisible('@deleteWebhookModalTitle');
+    client.pause(1000);
+    socketsPage
+      .clickButton('@confirmButton')
+      .waitForElementNotPresent('@selectWebhookTableRow');
+  }
+};
---
+++ tests/pages/socketsPage.js
@@ -0,0 +1,129 @@
+const globals = require('../globals');
+const utils = require('../utils');
+
+const dataCommands = {
+  clickButton(button) {
+    return this.waitForElementVisible(button)
+      .click(button);
+  },
+  selectFromDropdown(field, value) {
+    return this.waitForElementVisible(field)
+      .click(field)
+      .waitForElementVisible(value)
+      .click(value);
+  },
+  fillInputField(field, value) {
+    return this.waitForElementVisible(field)
+      .clearValue(field)
+      .setValue(field, value);
+  },
+  clickWebhookDropdown() {
+    return this.waitForElementVisible('@webhookDropdown').click('@webhookDropdown');
+  }
+};
+
+module.exports = {
+  url: 'https://localhost:8080/#/instances/' + globals.instanceName + '/sockets',
+  commands: [dataCommands],
+  elements: {
+    webhookDropdown: {
+      selector: '//span[@class="synicon-dots-vertical"]',
+      locateStrategy: 'xpath'
+    },
+    editDropdownItem: {
+      selector: '//a[@class="dropdown-item-edit"]',
+      locateStrategy: 'xpath'
+    },
+    instancesDropdown: {
+      selector: '.instances-dropdown'
+    },
+    cogIcon: {
+      selector: '.synicon-cog'
+    },
+    confirmButton: {
+      selector: '//span[text()="Confirm"]',
+      locateStrategy: 'xpath'
+    },
+    deleteButton: {
+      selector: '//span[@class="synicon-delete"]',
+      locateStrategy: 'xpath'
+    },
+    editButton: {
+      selector: '//span[@class="synicon-pencil"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookButton: {
+      selector: '.synicon-socket-codebox'
+    },
+    webhookListItem: {
+      selector: '//div[text()="webhook_description"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookModalTitle: {
+      selector: '//h3[text()="Add a Webhook"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookModalNameInput: {
+      selector: '//input[@name="name"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookModalDescriptionInput: {
+      selector: '//input[@name="description"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookModalCodeboxDropdown: {
+      selector: '//div[@class="codebox-dropdown"]',
+      locateStrategy: 'xpath'
+    },
+    addWebhookModalCodeboxDropdownChoice: {
+      selector: '//div[@class="codebox-dropdown"]//span[text()="codebox"]',
+      locateStrategy: 'xpath'
+    },
+    webhookTableRow: {
+      selector: '//div[text()="' + utils.addSuffix('webhook') + '"]',
+      locateStrategy: 'xpath'
+    },
+    selectWebhookTableRow: {
+      selector: '//div[text()="' + utils.addSuffix('webhook') + '"]/../div[1]/span',
+      locateStrategy: 'xpath'
+    },
+    webhookTableRowDescription: {
+      selector: '//div[text()="' + utils.addSuffix('webhook') + '"]/../following-sibling::div[1]',
+      locateStrategy: 'xpath'
+    },
+    deleteWebhookModalTitle: {
+      selector: '//h3[text()="Delete a Webhook"]',
+      locateStrategy: 'xpath'
+    },
+    editWebhookModalTitle: {
+      selector: '//h3[text()="Edit a Webhook"]',
+      locateStrategy: 'xpath'
+    },
+    dataListItem: {
+      selector: '//div[text()="data_view"]',
+      locateStrategy: 'xpath'
+    },
+    dataListItemTitle: {
+      selector: '//div[text()="Data Endpoints"]',
+      locateStrategy: 'xpath'
+    },
+    webhookToSelect: {
+      selector: '.col-xs-12 .synicon-arrow-up-bold'
+    },
+    checkboxToSelect: {
+      selector: '.synicon-checkbox-blank-outline'
+    },
+    checkboxSelected: {
+      selector: '.synicon-checkbox-marked-outline'
+    },
+    selectMultipleButton: {
+      selector: '.synicon-checkbox-multiple-marked-outline'
+    },
+    deselectMultipleButton: {
+      selector: '.synicon-checkbox-multiple-blank-outline'
+    },
+    emptyListItem: {
+      selector: '.empty-list-item'
+    }
+  }
+};
