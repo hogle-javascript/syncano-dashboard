43ddc76b6cb702ebb7fd87c324718b18b6b5c895
Daniel Kopka <daniel.kopka@syncano.com>
2015-11-19 14:16:11 +0100

    [DASH-738] it works

--- tests/instances/testTriggerTraces.js
+++ tests/instances/testTriggerTraces.js
@@ -4,83 +4,21 @@ import async from 'async';
 
 export default {
   tags: ['triggerTraces'],
-  before(client, done) {
-    const loginPage = client.page.loginPage();
-    const syncanoPage = client.page.syncanoPage();
+  after(client) {
+    client.end();
+  },
+  'Test Trigger Traces': (client) => {
+    const classesPage = client.page.classesPage();
 
     async.waterfall([
       client.createTempAccount,
       client.createTempClass
     ], (err) => {
       if (err) throw err;
-      console.log(globals);
-      done();
-    });
-
-    // async.waterfall([
-    //   client.createTempAccount,
-    //   client.createTempClass
-    // ], () => done());
-    // client.createTempClass();
-    // client.pause(5000);
-    // console.log(globals);
-    // syncanoPage.createTempClass(done.bind(null, true));
-
-    // client.session(function() {
-    //   const classOptions = {
-    //     name: 'class',
-    //     schema: [
-    //       {type: 'string', name: 'name'}
-    //     ]
-    //   };
-    //   const codeBoxOptions = {
-    //     label: 'codebox',
-    //     source: 'print "foo"',
-    //     runtime_name: 'python'
-    //   };
-    //   const triggerOptions = {
-    //     label: 'trigger',
-    //     signal: 'post_create',
-    //     class: 'class',
-    //     codebox: 1
-    //   };
-
-      // const syncano = new Syncano({accountKey: globals.tempAccountKey,
-      //                              baseUrl: 'https://api.syncano.rocks'});
-
-      // syncano.instance(globals.tempInstanceName).class().add(classOptions).then((res) => {
-      //   globals.tempClassName = res.name;
-      // });
-    //   syncano.instance(globals.tempInstanceName).codebox().add(codeBoxOptions).then((res) => {
-    //     globals.tempCodeBoxId = res.id;
-    //   });
-    //   syncano.instance(globals.tempInstanceName).trigger().add(triggerOptions).then((res) => {
-    //     console.log(res);
-    //     globals.tempTriggerLabel = res.label;
-    //   });
-
-    //   console.log(syncano.instance.class());
-    //   syncano.instance(globals.tempInstanceName).class(globals.tempClassName).dataobject().add({name: 'name'});
-    // });
-
-    //client.createTempClass(function(result) {
-    //  console.log(result);
-    //});
-    //loginPage.navigate();
-    // client.pause(5000);
-
-    //loginPage.login(globals.tempEmail, globals.tempPass)
-    //  .verifyLoginSuccessful();
-  },
-  after(client) {
-    client.end();
-  },
-  'Test Trigger Traces': (client) => {
-    const classesPage = client.page.classesPage();
-    console.log(globals);
-    client.url(`https://google.pl`);
-    client.pause(5000, function() {
-      console.log(globals);
+      client.url(`https://google.pl`);
+      client.pause(5000, function() {
+        console.log(globals);
+      });
     });
   }
 };
