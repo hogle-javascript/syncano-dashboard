9993808af2847e38c53265045c7bf0228e543940
Daniel Kopka <daniel.kopka@syncano.com>
2015-11-25 15:42:03 +0100

    [DASH-866] done

--- gulp-tasks/changelog.js
+++ gulp-tasks/changelog.js
@@ -2,40 +2,49 @@ var async   = require('async'),
     gutil   = require('gulp-util'),
     git     = require('gulp-git'),
     _       = require('lodash'),
-    request = require('superagent');
+    request = require('superagent'),
+    semver  = require('semver');
 
 module.exports = function(cb) {
-  var token = process.env.AHA_TOKEN;
-  var account = process.env.AHA_ACCOUNT;
+  var user = process.env.JIRA_USER;
+  var password = process.env.JIRA_PASSWORD;
+  var rootUrl = process.env.JIRA_ROOT_URL;
 
-  if (!token) {
-    throw new gutil.PluginError('changelog', '"AHA_TOKEN" env variable is required');
+  if (!rootUrl) {
+    throw new gutil.PluginError('changelog', '"JIRA_ROOT_URL" env variable is required');
   }
 
-  if (!account) {
-    throw new gutil.PluginError('changelog', '"AHA_ACCOUNT" env variable is required');
+  if (!user) {
+    throw new gutil.PluginError('changelog', '"JIRA_USER" env variable is required');
+  }
+
+  if (!password) {
+    throw new gutil.PluginError('changelog', '"JIRA_PASSWORD" env variable is required');
   }
 
   async.waterfall([
     function (callback) {
       // Fetch tags from origin
-      git.fetch('origin', '', {args: '--tags'}, callback);
+      git.fetch('origin', '', {args: ' --tags '}, callback);
     },
 
     function (callback) {
       // Grab list of tags
       git.exec({args: 'tag'}, function(err, stdout) {
         if (err) return callback(err);
-        var tags = stdout.split('\n').slice(-3);
-        callback(null, tags[0], tags[1]);
+        var tags = stdout.split('\n').filter(function(tag) {
+          return tag.length > 5;
+        });
+        tags = tags.sort(semver.rcompare);
+        callback(null, tags[1], tags[0]);
       });
     },
 
     function(start, end, callback) {
       // Parse git log in search of ticket numbers
       var range = start + '...' + end;
-      var command = 'log ' + range + ' --grep="[[:alpha:]{5, 10}][-][[:digit:]]" --oneline --pretty=format:"%s"';
-      var regex = /[A-Za-z]{5,10}-[\d]+/gm;
+      var command = 'log ' + range + ' --grep="[[:alpha:]{2, 10}][-][[:digit:]]" --oneline --pretty=format:"%s"';
+      var regex = /[A-Za-z]{2,10}-[\d]+/gm;
       git.exec({args: command}, function(err, stdout) {
         if (err) return callback(err);
         callback(null, _.uniq(stdout.match(regex)), end);
@@ -45,26 +54,26 @@ module.exports = function(cb) {
     function(tickets, tag, callback) {
       // Fetch tickets details from AHA
       async.mapLimit(tickets, 3, function(ticket, iteratorCallback) {
-        gutil.log('[changelog]', 'Fetching details for ' + ticket);
+        ticket = ticket.replace('FRONT-', 'DASH-').replace('SYNGUI-', 'DASH-');
+
+        gutil.log('[changelog]', 'Fetching details for ' + gutil.colors.blue(ticket));
         request
-          .get('https://' + account + '.aha.io/api/v1/features/' + ticket)
-          .set('Authorization', 'Basic ' + token)
+          .get(rootUrl + 'rest/api/2/issue/' + ticket)
+          .auth(user, password)
           .end(function(err, response) {
             if (err || response.statusCode !== 200) {
               return iteratorCallback(err, {
-                name: '',
+                summary: '',
                 type: 'Other',
-                resource: '',
-                reference_num: ticket
+                key: ticket
               })
             }
 
-            var feature = response.body.feature;
+            var issue = response.body.fields;
             iteratorCallback(null, {
-              name: feature.name,
-              type: feature.workflow_kind.name,
-              reference_num: feature.reference_num,
-              resource: feature.resource
+              summary: issue.summary,
+              type: issue.issuetype.name,
+              key: ticket
             })
           });
       }, function(err, results) {
@@ -76,14 +85,14 @@ module.exports = function(cb) {
 
     var groupedTickets = _.reduce(tickets, function(result, value) {
       result[value.type] = result[value.type] || [];
-      result[value.type].push('[' + value.reference_num + '] ' + value.name);
+      result[value.type].push('[' + gutil.colors.blue(value.key) + '] ' + value.summary);
       return result;
     }, {});
 
     console.log('\n\nChangelog for version:', tag + ':\n');
 
     _.forEach(groupedTickets, function(changelog, type) {
-      console.log(type + ':\n' + changelog.join('\n') + '\n\n');
+      console.log(gutil.colors.green.bold(type) + ':\n' + changelog.join('\n') + '\n\n');
     });
 
     cb();
