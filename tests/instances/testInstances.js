d152f3f4fe5b4a761f0ca840aab58594f42f14a1
Patryk Kopycinski <contact@patrykkopycinski.com>
2015-12-04 05:39:17 +0100

    [DASH-818] Disable InstancesDropdown test

--- tests/instances/testInstances.js
+++ tests/instances/testInstances.js
@@ -100,35 +100,35 @@ export default {
     }
     instancesPage.expect.element('@instancesTableRow').to.be.present.after(10000);
   },
-  'Test Instances Dropdown': (client) => {
-    const instancesPage = client.page.instancesPage();
-    const leftMenuPage = client.page.leftMenuPage();
-    const socketsPage = client.page.socketsPage();
-
-    const instanceNames = [];
-
-    instancesPage.navigate();
-    instancesPage.waitForElementPresent('@instancesTableName');
-    const instanceName = instancesPage.elements.instancesTableName;
-
-    client.elements(instanceName.locateStrategy, instanceName.selector, (result) => {
-      result.value.forEach((value) => {
-        client.elementIdText(value.ELEMENT, (el) => {
-          instanceNames.push(el.value);
-        });
-      });
-    });
-    instancesPage.waitForElementPresent('@instancesTableRow');
-    instancesPage.clickButton('@instancesTableRow');
-    leftMenuPage.clickButton('@instancesDropdown');
-    leftMenuPage.clickButton('@instancesListSecondItem');
-    socketsPage.waitForElementPresent('@dataSocketTableTitle');
-    const dropdown = leftMenuPage.elements.instancesDropdownName.selector;
-
-    client.getText('xpath', dropdown, (text) => {
-      client.assert.equal(text.value, instanceNames[0]);
-    });
-  },
+  //'Test Instances Dropdown': (client) => {
+  //  const instancesPage = client.page.instancesPage();
+  //  const leftMenuPage = client.page.leftMenuPage();
+  //  const socketsPage = client.page.socketsPage();
+  //
+  //  const instanceNames = [];
+  //
+  //  instancesPage.navigate();
+  //  instancesPage.waitForElementPresent('@instancesTableName');
+  //  const instanceName = instancesPage.elements.instancesTableName;
+  //
+  //  client.elements(instanceName.locateStrategy, instanceName.selector, (result) => {
+  //    result.value.forEach((value) => {
+  //      client.elementIdText(value.ELEMENT, (el) => {
+  //        instanceNames.push(el.value);
+  //      });
+  //    });
+  //  });
+  //  instancesPage.waitForElementPresent('@instancesTableRow');
+  //  instancesPage.clickButton('@instancesTableRow');
+  //  leftMenuPage.clickButton('@instancesDropdown');
+  //  leftMenuPage.clickButton('@instancesListSecondItem');
+  //  socketsPage.waitForElementPresent('@dataSocketTableTitle');
+  //  const dropdown = leftMenuPage.elements.instancesDropdownName.selector;
+  //
+  //  client.getText('xpath', dropdown, (text) => {
+  //    client.assert.equal(text.value, instanceNames[0]);
+  //  });
+  //},
   'Test Delete multiple Instances': (client) => {
     const instancesPage = client.page.instancesPage();
 
