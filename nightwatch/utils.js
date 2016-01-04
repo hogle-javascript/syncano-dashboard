d839f0192b72b1142c3f997d03845015fb26f4cf
Adam Wardecki <adam.wardecki@gmail.com>
2016-01-04 11:19:59 +0100

    [DASH-950] Addressing comments

--- nightwatch/utils.js
+++ nightwatch/utils.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 export default {
   suffix: '_' + new Date().getTime() + _.random(0, 99999),
-  addSuffix: function addSuffix(text) {
+  addSuffix(text) {
     if (typeof text === 'undefined') {
       return this.suffix;
     }
