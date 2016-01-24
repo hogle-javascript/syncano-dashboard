85ac8b73a27210a5774bae5c31268eef323a6ba4
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-01-24 13:17:54 +0100

    [DASH-27] Refactor SocialAuthButtonsList

--- src/common/SocialAuthButtonsList/SocialAuthButtonsList.jsx
+++ src/common/SocialAuthButtonsList/SocialAuthButtonsList.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import Radium from 'radium';
-import {State} from 'react-router';
 
-import SessionStore from '../../apps/Session/SessionStore';
-import AuthActions from '../../apps/Account/AuthActions';
-import AuthConstants from '../../apps/Account/AuthConstants';
+import ListItem from './SocialAuthButtonsListItem';
+import ListItemDivider from './SocialAuthButtonsListItemDivider';
+import {Styles, List} from 'syncano-material-ui';
 
-import {Styles, FontIcon, Divider, List, ListItem} from 'syncano-material-ui';
-
-export default Radium(React.createClass({
-
+export default React.createClass({
   displayName: 'SocialAuthButtonsList',
-
-  mixins: [
-    State
-  ],
 
   getDefaultProps() {
     return {
@@ -24,61 +15,28 @@ export default Radium(React.createClass({
 
   getStyles() {
     return {
-      list: {
-        paddingTop: 0,
-        paddingBottom: 0,
-        marginBottom: 24,
-        border: `1px solid ${Styles.Colors.blue700}`
-      },
-      listItem: {
-        color: Styles.Colors.blue700,
-        ':hover': {
-          backgroundColor: Styles.Colors.blue700,
-          color: '#fff'
-        }
-      },
-      listItemIcon: {
-        color: 'inherit',
-        transition: 'none',
-        left: 12
-      },
-      listDivider: {
-        backgroundColor: Styles.Colors.blue700
-      }
+      paddingTop: 0,
+      paddingBottom: 0,
+      marginBottom: 24,
+      border: `1px solid ${Styles.Colors.blue700}`
     };
   },
 
-  handleSocialSignup(network) {
-    if (this.isActive('signup')) {
-      SessionStore.showWelcomeDialog();
-    }
-
-    SessionStore.setSignUpMode();
-    AuthActions.socialLogin(network);
-  },
-
   renderSocialButtons() {
-    let styles = this.getStyles();
-    let socialNetworksCount = AuthConstants.SOCIAL_NETWORKS.length;
-    let lastListItemIndex = socialNetworksCount - 1;
-    let buttonLabel = this.props.mode === 'signup' ? 'Sign up with ' : 'Login with ';
+    let {networks, mode, onSocialLogin} = this.props;
+    let lastListItemIndex = networks.length - 1;
     let buttons = [];
 
-    AuthConstants.SOCIAL_NETWORKS.map((network, index) => {
+    networks.map((network, index) => {
       buttons.push(
         <ListItem
-          key={`network-${index}`}
-          style={styles.listItem}
-          primaryText={buttonLabel + network}
-          onTouchTap={this.handleSocialSignup.bind(null, network)}
-          leftIcon={<FontIcon
-                      style = {styles.listItemIcon}
-                      className = {`synicon-${network}`}/>}/>
+          key={`network-${network}`}
+          network={network}
+          mode={mode}
+          onTouchTap={() => onSocialLogin(network)} />
       );
       if (index < lastListItemIndex) {
-        buttons.push(<Divider
-          key={`divider-${index}`}
-          style={styles.listDivider}/>);
+        buttons.push(<ListItemDivider key={`divider-${index}`} />);
       }
     });
 
@@ -89,7 +47,7 @@ export default Radium(React.createClass({
     let styles = this.getStyles();
 
     return (
-      <List style={styles.list}>{this.renderSocialButtons()}</List>
+      <List style={styles}>{this.renderSocialButtons()}</List>
     );
   }
-}));
+});
