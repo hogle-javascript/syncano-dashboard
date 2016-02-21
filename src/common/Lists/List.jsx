548014f6afe7af61aa39eb49ffcb99edb6e938cf
Daniel Kopka <daniel.kopka@syncano.com>
2016-02-21 23:09:03 +0100

    [DASH-958] Cleanup

--- src/common/Lists/List.jsx
+++ src/common/Lists/List.jsx
@@ -13,10 +13,8 @@ export default React.createClass({
       return this.props.children;
     }
 
-    let items = this.props.items.map((item, index) => this.props.renderItem(item, index));
-
-    if (items.length > 0) {
-      return items;
+    if (this.props.items.length > 0) {
+      return this.props.items.map((item, index) => this.props.renderItem(item, index));
     }
 
     return (
