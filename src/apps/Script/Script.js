435f964b04491a5187c54712b9a93ad5811eda7c
Daniel Kopka <daniel.kopka@syncano.com>
2016-03-10 13:51:34 +0100

    [DASH-1124] Change default payload in scripts to something dummy

--- src/apps/Script/Script.js
+++ src/apps/Script/Script.js
@@ -213,9 +213,8 @@ export default React.createClass({
                     onChange={(payload) => this.setState({payloadValue: payload})}
                     value={[
                       '{',
-                      '    "language": "JSON",',
                       '    "foo": "bar",',
-                      '    "trailing": "comma"',
+                      '    "bar": "foo"',
                       '}'
                     ].join('\n')} />
                 </TogglePanel>
