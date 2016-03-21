40df5b0e0af4785833a9ee7489ae20dfdcadb23b
Daniel Kopka <daniel.kopka@syncano.com>
2016-03-21 09:01:45 +0100

    [DASH-1233] Batch requests for sockets screen

--- src/apps/Syncano/Actions/Sockets.js
+++ src/apps/Syncano/Actions/Sockets.js
@@ -3,35 +3,53 @@ import _ from 'lodash';
 export default {
   list(params = {}) {
     _.defaults(params, {ordering: 'desc'});
-    const promises = [
-      this.Connection.DataViews.list(params),
-      this.Connection.WebHooks.list(params),
-      this.Connection.Triggers.list(params),
-      this.Connection.Schedules.list(params),
-      this.Connection.Channels.list(params),
-      this.Connection.PushNotifications.GCM.get(),
-      this.Connection.PushNotifications.APNS.get(),
-      this.Connection.PushNotifications.Devices.list('gcm'),
-      this.Connection.PushNotifications.Devices.list('apns'),
-      this.Connection.CodeBoxes.list(params),
-      this.Connection.Classes.list(params)
+
+    const {instance} = this.Connection.getInfo();
+    const keys = [
+      'data',
+      'scriptEndpoints',
+      'triggers',
+      'schedules',
+      'channels',
+      'gcmPushNotifications',
+      'apnsPushNotifications',
+      'gcmDevices',
+      'apnsDevices',
+      'scripts',
+      'classes'
+    ];
+    const requests = [
+      {method: 'GET', path: `/v1/instances/${instance.name}/api/objects/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/webhooks/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/triggers/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/schedules/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/channels/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/push_notifications/gcm/config/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/push_notifications/apns/config/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/push_notifications/gcm/devices/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/push_notifications/apns/devices/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/codeboxes/`},
+      {method: 'GET', path: `/v1/instances/${instance.name}/classes/`}
     ];
 
-    this.Promise.all(promises)
-      .then((sockets) => {
-        return {
-          data: sockets[0],
-          scriptEndpoints: sockets[1],
-          triggers: sockets[2],
-          schedules: sockets[3],
-          channels: sockets[4],
-          gcmPushNotifications: sockets[5],
-          apnsPushNotifications: sockets[6],
-          gcmDevices: sockets[7],
-          apnsDevices: sockets[8],
-          scripts: sockets[9],
-          classes: sockets[10]
-        };
+    this.Connection
+      .request('POST', `v1/instances/${instance.name}/batch/`, {requests, serialize: false})
+      .then((responses) => {
+        return _.reduce(responses, (result, response, index) => {
+          const key = keys[index];
+
+          if (response.code === 200) {
+            if (key === 'gcmPushNotifications' || key === 'apnsPushNotifications') {
+              result[key] = response.content;
+            } else {
+              result[key] = this.Connection.createList(this.Connection, response.content);
+            }
+          } else {
+            result[key] = this.Connection.createList(this.Connection, {objects: []});
+          }
+
+          return result;
+        }, {});
       })
       .then(this.completed)
       .catch(this.failure);
