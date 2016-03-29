cd2b505a2822b25103bc23c1c2008fd480a50936
Adam Wardecki <adam.wardecki@gmail.com>
2016-03-29 17:48:35 +0200

    [DASH-1202] Make text for script endpoint

--- src/apps/ScriptEndpoints/ScriptEndpointDialog.jsx
+++ src/apps/ScriptEndpoints/ScriptEndpointDialog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {State, Navigation} from 'react-router';
 import Reflux from 'reflux';
 
 // Utils
@@ -20,7 +21,9 @@ export default React.createClass({
   mixins: [
     Reflux.connect(DialogStore),
     DialogMixin,
-    FormMixin
+    FormMixin,
+    State,
+    Navigation
   ],
 
   validatorConstraints: {
@@ -78,38 +81,68 @@ export default React.createClass({
           <Dialog.StandardButtons
             handleCancel={this.handleCancel}
             handleConfirm={this.handleFormValidation}/>
+        }
+        sidebar={
+          <Dialog.SidebarBox>
+            <Dialog.SidebarSection>
+              Script Endpoints are URLs that run your custom code in the cloud.
+              This code can be created with Scripts.
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection title="Scripts">
+              A Script is an object that contains a piece of code that can be run on Syncano servers.
+              If you haven&#39;t created one you can do so&nbsp;
+              <a
+                 href={this.makeHref('scripts', this.getParams())}
+                 style={{textDecoration: 'underline', color: 'rgba(68,68,68,.5)'}}>
+                 <strong>here.</strong>
+              </a>
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection title="Public Script Endpoints">
+              They are accessible to anyone who knows the Script Endpoint URL.
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection last={true}>
+              <a
+                href="http://docs.syncano.io/docs/endpoints-scripts"
+                style={{textDecoration: 'underline', color: 'rgba(68,68,68,.5)'}}
+                target="_blank">
+                <strong>Learn more</strong>
+              </a>
+            </Dialog.SidebarSection>
+          </Dialog.SidebarBox>
         }>
         {this.renderFormNotifications()}
-        <TextField
-          ref="name"
-          name="name"
-          fullWidth={true}
-          disabled={this.hasEditMode()}
-          valueLink={this.linkState('name')}
-          errorText={this.getValidationMessages('name').join(' ')}
-          hintText="Script Endpoint's name"
-          floatingLabelText="Name"/>
-        <TextField
-          ref="description"
-          name="description"
-          fullWidth={true}
-          valueLink={this.linkState('description')}
-          errorText={this.getValidationMessages('description').join(' ')}
-          hintText="Script Endpoint's description"
-          floatingLabelText="Description (optional)"/>
-        <SelectFieldWrapper
-          name="script"
-          options={this.state.scripts}
-          value={this.state.codebox}
-          onChange={this.setSelectFieldValue.bind(null, 'codebox')}
-          errorText={this.getValidationMessages('codebox').join(' ')}/>
-        <Toggle
-          ref='public'
-          name='public'
-          onToggle={this.handleToogle}
-          style={{marginTop: 20}}
-          defaultToggled={this.state.public}
-          label='Make this Script Endpoint public?'/>
+        <Dialog.ContentSection>
+          <TextField
+            ref="name"
+            name="name"
+            fullWidth={true}
+            disabled={this.hasEditMode()}
+            valueLink={this.linkState('name')}
+            errorText={this.getValidationMessages('name').join(' ')}
+            hintText="Script Endpoint's name"
+            floatingLabelText="Name"/>
+          <TextField
+            ref="description"
+            name="description"
+            fullWidth={true}
+            valueLink={this.linkState('description')}
+            errorText={this.getValidationMessages('description').join(' ')}
+            hintText="Script Endpoint's description"
+            floatingLabelText="Description (optional)"/>
+          <SelectFieldWrapper
+            name="script"
+            options={this.state.scripts}
+            value={this.state.codebox}
+            onChange={this.setSelectFieldValue.bind(null, 'codebox')}
+            errorText={this.getValidationMessages('codebox').join(' ')}/>
+          <Toggle
+            ref='public'
+            name='public'
+            onToggle={this.handleToogle}
+            style={{marginTop: 20}}
+            defaultToggled={this.state.public}
+            label='Make this Script Endpoint public?'/>
+        </Dialog.ContentSection>
       </Dialog.FullPage>
     );
   }
