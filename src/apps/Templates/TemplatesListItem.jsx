17a8d643c5064fdc1d95701801ce56e1e3f8d0e8
Hubert Wesołowski <hwesol13@gmail.com>
2016-04-06 14:01:21 +0200

    [DASH-1312] change to stateless component

--- src/apps/Templates/TemplatesListItem.jsx
+++ src/apps/Templates/TemplatesListItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {State} from 'react-router';
 import _ from 'lodash';
 
 import Actions from './TemplatesActions';
+import SessionStore from '../Session/SessionStore';
 
 import {MenuItem} from 'syncano-material-ui';
 import {Color, ColumnList, Truncate} from 'syncano-components';
@@ -10,47 +10,39 @@ import {LinkWrapper} from '../../common/';
 
 const Column = ColumnList.Column;
 
-export default React.createClass({
-  displayName: 'TemplatesListItem',
-
-  mixins: [State],
-
-  render() {
-    const {item, onIconClick, showDeleteDialog} = this.props;
-
-    return (
-      <ColumnList.Item
-        checked={item.checked}
+export default ({item, onIconClick, showDeleteDialog}) => {
+  return (
+    <ColumnList.Item
+      checked={item.checked}
+      id={item.name}
+      key={item.name}>
+      <Column.CheckIcon
+        className="col-xs-12"
         id={item.name}
-        key={item.name}>
-        <Column.CheckIcon
-          className="col-xs-12"
-          id={item.name}
-          iconClassName="arrow-up-bold"
-          keyName="name"
-          background={Color.getColorByName('blue', 'xlight')}
-          checked={item.checked}
-          handleIconClick={onIconClick}
-          primaryText={
-            <LinkWrapper
-              to="template"
-              params={_.merge({}, this.getParams(), {templateName: item.name})}>
-              <Truncate text={item.name}/>
-            </LinkWrapper>
-          }>
-        </Column.CheckIcon>
-        <Column.Desc className="col-flex-1">{item.content_type}</Column.Desc>
-        <Column.Menu>
-          <MenuItem
-            className="dropdown-item-edit"
-            onTouchTap={() => Actions.showDialog(item)}
-            primaryText="Edit a Template" />
-          <MenuItem
-            className="dropdown-item-delete"
-            onTouchTap={showDeleteDialog}
-            primaryText="Delete a Template" />
-        </Column.Menu>
-      </ColumnList.Item>
-    );
-  }
-});
+        iconClassName="arrow-up-bold"
+        keyName="name"
+        background={Color.getColorByName('blue', 'xlight')}
+        checked={item.checked}
+        handleIconClick={onIconClick}
+        primaryText={
+          <LinkWrapper
+            to="template"
+            params={_.merge({}, SessionStore.router.getCurrentParams(), {templateName: item.name})}>
+            <Truncate text={item.name}/>
+          </LinkWrapper>
+        }>
+      </Column.CheckIcon>
+      <Column.Desc className="col-flex-1">{item.content_type}</Column.Desc>
+      <Column.Menu>
+        <MenuItem
+          className="dropdown-item-edit"
+          onTouchTap={() => Actions.showDialog(item)}
+          primaryText="Edit a Template" />
+        <MenuItem
+          className="dropdown-item-delete"
+          onTouchTap={showDeleteDialog}
+          primaryText="Delete a Template" />
+      </Column.Menu>
+    </ColumnList.Item>
+  );
+};
