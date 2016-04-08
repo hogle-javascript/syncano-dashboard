fae149eef237b2a4f72f897290c85fc67573a969
Hubert Wesołowski <hwesol13@gmail.com>
2016-04-08 12:16:03 +0200

    [DASH-1204] Text for schedule socket dialog

--- src/apps/Schedules/ScheduleDialog.jsx
+++ src/apps/Schedules/ScheduleDialog.jsx
@@ -92,6 +92,26 @@ export default React.createClass({
           <Dialog.StandardButtons
             handleCancel={this.handleCancel}
             handleConfirm={this.handleFormValidation}/>
+        }
+        sidebar={
+          <Dialog.SidebarBox>
+            <Dialog.SidebarSection>
+              Schedule Sockets are one of the available ways of running your Snippet Scripts. Thanks to Schedules
+               you can execute Scripts at some time interval.
+               (e.g. every 5 minutes).
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection title="Script">
+              Chosen Script will be executed at selected time interval writen as crontab.
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection title="Crontab">
+              We prepared some crontabs for you so you can choose from predefined or write your own.
+            </Dialog.SidebarSection>
+            <Dialog.SidebarSection last={true}>
+              <Dialog.SidebarLink to="http://docs.syncano.io/docs/schedules">
+                Learn more
+              </Dialog.SidebarLink>
+            </Dialog.SidebarSection>
+          </Dialog.SidebarBox>
         }>
         <div>
           {this.renderFormNotifications()}
