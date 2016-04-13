161a462a06c68f2f9326ff6714b7b733003aba50
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-04-13 21:10:15 +0200

    [DASH-1305] Reverse the ordering of invoices list

--- src/apps/Profile/ProfileBillingInvoices.jsx
+++ src/apps/Profile/ProfileBillingInvoices.jsx
@@ -5,7 +5,7 @@ import SessionStore from '../Session/SessionStore';
 import Actions from './ProfileActions';
 import Store from './ProfileBillingInvoicesStore';
 
-import {FlatButton} from 'syncano-material-ui';
+import {RaisedButton} from 'syncano-material-ui';
 import {ColumnList, Container, Loading, Show} from 'syncano-components';
 import {InnerToolbar, Lists} from '../../common';
 
@@ -29,49 +29,47 @@ export default React.createClass({
     location.href = pdfUrl;
   },
 
-  renderListItem(invoice) {
-    return (
-      <ColumnList.Item key={invoice.id}>
-        <Column.Desc>{invoice.period}</Column.Desc>
-        <Column.Desc>{invoice.id}</Column.Desc>
-        <Column.Desc>{invoice.amount}</Column.Desc>
-        <Column.Desc>{invoice.status}</Column.Desc>
-        <Column.Desc>
-          <FlatButton
-            label="VIEW"
-            primary={true}
-            onClick={this.handlePDFClick.bind(null, invoice)}/>
-        </Column.Desc>
-      </ColumnList.Item>
-    );
-  },
-
   render() {
+    const {isLoading, invoices} = this.state;
+
     return (
-      <Loading show={this.state.isLoading}>
+      <Loading show={isLoading}>
         <InnerToolbar title="Invoices"/>
 
-        <Show if={this.state.invoices.length < 1}>
+        <Show if={invoices.length < 1}>
           <Container.Empty
-            icon='synicon-file-outline'
-            text='You have no invoices'/>
+            icon="synicon-file-outline"
+            text="You have no invoices"/>
         </Show>
 
-        <Show if={this.state.invoices.length > 0}>
-          <Lists.Container
-            style={{width: '100%', margin: '65px 0px 0px 0px'}}
-            className='invoices-list'>
-            <ColumnList.Header>
-              <Column.ColumnHeader columnName="DESC">Period</Column.ColumnHeader>
-              <Column.ColumnHeader columnName="DESC">Invoice ID</Column.ColumnHeader>
-              <Column.ColumnHeader columnName="DESC">Amount</Column.ColumnHeader>
-              <Column.ColumnHeader columnName="DESC">Status</Column.ColumnHeader>
-              <Column.ColumnHeader columnName="DESC">Action</Column.ColumnHeader>
-            </ColumnList.Header>
-            <Lists.List key="invoices-list">
-              {this.state.invoices.map(this.renderListItem)}
-            </Lists.List>
-          </Lists.Container>
+        <Show if={invoices.length > 0}>
+          <Container>
+            <Lists.Container className='invoices-list'>
+              <ColumnList.Header>
+                <Column.ColumnHeader columnName="DESC">Period</Column.ColumnHeader>
+                <Column.ColumnHeader columnName="DESC">Invoice ID</Column.ColumnHeader>
+                <Column.ColumnHeader columnName="DESC">Amount</Column.ColumnHeader>
+                <Column.ColumnHeader columnName="DESC">Status</Column.ColumnHeader>
+                <Column.ColumnHeader columnName="DESC">Action</Column.ColumnHeader>
+              </ColumnList.Header>
+              <Lists.List key="invoices-list">
+                {invoices.map((invoice) => (
+                  <ColumnList.Item key={invoice.id}>
+                    <Column.Desc>{invoice.period}</Column.Desc>
+                    <Column.Desc>{invoice.id}</Column.Desc>
+                    <Column.Desc>{invoice.amount}</Column.Desc>
+                    <Column.Desc>{invoice.status}</Column.Desc>
+                    <Column.Desc>
+                      <RaisedButton
+                        label="VIEW"
+                        primary={true}
+                        onClick={() => this.handlePDFClick(invoice)}/>
+                    </Column.Desc>
+                  </ColumnList.Item>
+                ))}
+              </Lists.List>
+            </Lists.Container>
+          </Container>
         </Show>
       </Loading>
     );
