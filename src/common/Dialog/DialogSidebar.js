8fb1f74e0a46eb4a1ccd16f92cb2a0e19252db61
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-04-17 10:31:46 +0200

    [DASH-1209] Make text for instance dialog

--- src/common/Dialog/DialogSidebar.js
+++ src/common/Dialog/DialogSidebar.js
@@ -3,17 +3,26 @@ import React from 'react';
 export default ({children}) => {
   const styles = {
     root: {
-      width: 238,
+      width: 238 * React.Children.count(children),
       paddingTop: 16,
-      paddingRight: 20
+      paddingRight: 20,
+      display: 'flex',
+      flexDirection: 'row'
+    },
+    column: {
+      display: 'flex'
     }
   };
 
   return (
-    <div
-      className="col-flex-0"
-      style={styles.root}>
-      {children}
+    <div style={styles.root}>
+      {React.Children.map(children, (child) => (
+        <div
+          className="col-flex-0"
+          style={styles.column}>
+          {child}
+        </div>
+      ))}
     </div>
   );
 };
