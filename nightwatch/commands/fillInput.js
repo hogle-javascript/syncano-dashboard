fcbcc9020b2fb7ded881be326579dc8cc14bd670
Marcin Godlewski <marcin.godlewski@syncano.com>
2016-04-18 14:09:44 +0200

    [DASH-1323] Add pause after clearing input, as some tests appended the field without clearing it.

--- nightwatch/commands/fillInput.js
+++ nightwatch/commands/fillInput.js
@@ -2,6 +2,7 @@ exports.command = function fillInput(element, string) {
   this
     .waitForElementVisible(element)
     .clearValue(element)
+    .pause(1000)
     .setValue(element, string)
     .pause(1000);
 
