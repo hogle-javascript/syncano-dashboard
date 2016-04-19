93588c80342bd5dc606ba00e6840ad2a7927f9c5
Marcin Godlewski <marcin.godlewski@syncano.com>
2016-04-19 14:08:01 +0200

    [DASH-1323] Change before and after action to be executed after every testcase

--- nightwatch/commands/fillInput.js
+++ nightwatch/commands/fillInput.js
@@ -2,7 +2,6 @@ exports.command = function fillInput(element, string) {
   this
     .waitForElementVisible(element)
     .clearValue(element)
-    .pause(1000)
     .setValue(element, string)
     .pause(1000);
 
