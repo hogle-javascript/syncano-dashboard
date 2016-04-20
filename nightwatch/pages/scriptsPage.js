8678656d17aaf6f5c7e6ecde6945c94b7a183025
Marcin Godlewski <marcin.godlewski@syncano.com>
2016-04-20 15:52:43 +0200

    [DASH-1326] Add url for scriptsPage

--- nightwatch/pages/scriptsPage.js
+++ nightwatch/pages/scriptsPage.js
@@ -1,4 +1,8 @@
+import utils from '../utils';
+import globals from '../globals';
+
 export default {
+  url: `https://localhost:8080/#/instances/${globals.instanceName}/scripts`,
   elements: {
     scriptListItem: {
       selector: '//div[text()="snippet"]',
