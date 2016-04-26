1f722e4c1609bcd7cced063955a43257b4a3858e
Marcin Godlewski <marcin.godlewski@syncano.com>
2016-04-26 10:51:19 +0200

    [DASH-1326] Add todo to not forget about deleting pauses after front fix

--- nightwatch/tests/instances/testScripts.js
+++ nightwatch/tests/instances/testScripts.js
@@ -25,6 +25,7 @@ export default {
     const scriptsPage = client.page.scriptsPage();
     const tempUrl = `https://localhost:8080/#/instances/${Globals.tempInstanceName}/scripts`;
 
+    // ToDO: Delete client.pause after dissappearing of scripts will be solved
     client
       .pause(2000)
       .url(tempUrl);
