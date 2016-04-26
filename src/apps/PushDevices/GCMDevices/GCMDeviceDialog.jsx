6cc87c6b68d1fe46e8bdacc3e88261feb5044411
Hubert Wesołowski <hwesol13@gmail.com>
2016-04-26 12:48:09 +0200

    [DASH-1362] Small improvement

--- src/apps/PushDevices/GCMDevices/GCMDeviceDialog.jsx
+++ src/apps/PushDevices/GCMDevices/GCMDeviceDialog.jsx
@@ -11,8 +11,11 @@ const sidebar = (
       <Dialog.SidebarSection>
         GCM Devices allows you to send Push Notifications to your Android devices.
       </Dialog.SidebarSection>
+      <Dialog.SidebarSection>
+        <i>NOTE: At least one production or development API key must be added.</i>
+      </Dialog.SidebarSection>
       <Dialog.SidebarSection title="Registration ID">
-        To send and receive push notifications your app must register with GCM. In this process, the client gets a
+        To send and receive Push Notifications your app must register with GCM. In this process, the client gets a
          unique registration ID. Read more at&nbsp;
         <Dialog.SidebarLink to="https://console.developers.google.com">
           Google Developer Console.
