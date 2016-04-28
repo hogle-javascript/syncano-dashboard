ff6f2d6a507d4f42ce368a5be8b80b1702aa4159
Marcin Godlewski <gher13@gmail.com>
2016-04-28 14:39:42 +0200

    [DASH-1380] Create e2e tests for profile updating (#1246)
    * [DASH-1380] Create e2e tests for profile updating
    
    * [DASH-1380] Delete unused locator
    
    * [DASH-1380] Change logic to wait for success dialog then verify values of fields
    
    * [DASH-1380] Change tests to wait before inputing values

--- nightwatch/pages/profilePage.js
+++ nightwatch/pages/profilePage.js
@@ -2,18 +2,22 @@ export default {
   url: 'https://localhost:8080/#/account',
   elements: {
     updateButton: {
-      selector: '//button[@class="raised-button"]',
+      selector: '//span[text()="Update"]',
       locateStrategy: 'xpath'
     },
     title: {
-      selector: '//div[@class="container"]//div[text()="Profile"]',
+      selector: '//span[text()="Profile"]',
       locateStrategy: 'xpath'
     },
     firstName: {
       selector: 'input[name=firstName]'
     },
-    email: {
-      selector: 'input[name=email]'
+    lastName: {
+      selector: 'input[name=lastName]'
+    },
+    successDialog: {
+      selector: '//div[text()="Profile saved successfully."]',
+      locateStrategy: 'xpath'
     }
   }
 };
---
+++ nightwatch/tests/settings/testProfileChange.js
@@ -0,0 +1,34 @@
+import Utils from '../../utils';
+
+export default {
+  tags: ['profile'],
+  after(client) {
+    client.end();
+  },
+  before(client) {
+    const loginPage = client.page.loginPage();
+
+    loginPage
+      .navigate()
+      .login(process.env.NIGHTWATCH_EMAIL, process.env.NIGHTWATCH_PASSWORD);
+  },
+  'Administrator changes his name and surname': (client) => {
+    const profilePage = client.page.profilePage();
+    const firstName = Utils.addSuffix('name');
+    const lastName = Utils.addSuffix('surname');
+
+    profilePage
+      .navigate()
+      .waitForElementPresent('@title');
+
+    client.pause(2000);
+
+    profilePage
+      .fillInput('@firstName', firstName)
+      .fillInput('@lastName', lastName)
+      .clickElement('@updateButton')
+      .waitForElementPresent('@successDialog')
+      .verify.value('@firstName', firstName)
+      .verify.value('@lastName', lastName);
+  }
+};
