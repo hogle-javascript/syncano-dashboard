f9b5d047dbfa727cec28e9be6c30792ca1eaf35b
Hubert Wesołowski <hwesol13@gmail.com>
2016-05-05 13:21:51 +0200

    [DASH-777] migration to new Syncano js lib - Admins

--- src/apps/Syncano/Actions/Admins.js
+++ src/apps/Syncano/Actions/Admins.js
@@ -1,25 +1,24 @@
-import _ from 'lodash';
-
 export default {
-  list(params = {}) {
-    _.defaults(params, {ordering: 'desc'});
-    this.Connection
-      .Admins
-      .list(params)
+  list() {
+    this.NewLibConnection
+      .Admin
+      .please()
+      .list()
+      .ordering('desc')
       .then(this.completed)
       .catch(this.failure);
   },
 
-  update(name, payload) {
+  update(id, payload) {
     this.Connection
       .Admins
-      .update(name, payload)
+      .update(id, payload)
       .then(this.completed)
       .catch(this.failure);
   },
 
   remove(admins) {
-    let promises = admins.map((admin) => this.Connection.Admins.remove(admin));
+    const promises = admins.map((admin) => this.Connection.Admins.remove(admin));
 
     this.Promise.all(promises)
       .then(this.completed)
