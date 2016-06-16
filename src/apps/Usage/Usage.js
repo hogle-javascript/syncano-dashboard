b58b3aab9797d3a3b04c1bbdbd5f0f1ec89722ce
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-06-16 18:25:26 +0200

    [DASH-1622] Convert Usage to stateless component

--- src/apps/Usage/Usage.js
+++ src/apps/Usage/Usage.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
-
-// Components
 import { Container, InnerToolbar } from '../../common/';
 import Chart from '../Profile/ProfileBillingChart';
 
-class Usage extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet title="Usage" />
-        <InnerToolbar title="Usage" />
-        <Container>
-          <Chart />
-        </Container>
-      </div>
-    );
-  }
-}
+const Usage = () => (
+  <div>
+    <Helmet title="Usage" />
+    <InnerToolbar title="Usage" />
+    <Container>
+      <Chart />
+    </Container>
+  </div>
+);
 
 export default Usage;
