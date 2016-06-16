c0eb686e55d4614e970693e1abe585a7572922b4
Adam Wardecki <adam.wardecki@gmail.com>
2016-06-16 16:02:02 +0200

    [DASH-1567] Couple grammar fixes

--- src/apps/DataEndpoints/DataEndpointSummaryDialog.js
+++ src/apps/DataEndpoints/DataEndpointSummaryDialog.js
@@ -32,11 +32,12 @@ export default React.createClass({
     const token = SessionStore.getToken();
     const currentInstance = SessionStore.getInstance();
 
+    console.log(token);
     return (
       <Dialog.FullPage
         key="dialog"
         ref="dialog"
-        title="Hooray! You've just created a Data Endpoint!"
+        title="You've just created a Data Endpoint!"
         titleStyle={{paddingLeft: 72}}
         onRequestClose={this.handleCancel}
         loading={dataEndpoints.isLoading}
@@ -55,9 +56,9 @@ export default React.createClass({
               <div className="col-flex-1">
                 <div style={{fontSize: 16, lineHeight: 1.6, color: 'rgba(68,68,68, .8)'}}>
                   <p>
-                    Data Endpoint you just created, can always be modified later. You can change which fields from your
-                    objects should be hidden, which should stay visible, which reference fields to expand and how should
-                    look like the search query applied to it.
+                    Data Endpoint you just created can always be modified later. You can change which Data Object fields
+                    should be hidden, which should stay visible and which reference fields to expand. You
+                    can also apply search queries on top of filtering.
                   </p>
                 </div>
               </div>
@@ -65,7 +66,7 @@ export default React.createClass({
             <Dialog.ContentSection>
               <div className="col-flex-1">
                 <Notification>
-                  You have chosen to use class "{item.class}", but it does not contain any custom fields in the class
+                  You have chosen the "{item.class}" Class but it doesn't contain any custom fields in the Class
                   schema. To use the full power of Data Endpoints, we suggest
                   <Link
                     to={{
@@ -73,7 +74,7 @@ export default React.createClass({
                       params: {...params, className: item.class, action: 'edit'}
                     }}
                     style={{fontWeight: 700}}>
-                    {` clicking here to add custom fields for your data objects.`}
+                    {` clicking here to add custom fields for your Data Objects.`}
                   </Link>
                 </Notification>
               </div>
@@ -85,7 +86,7 @@ export default React.createClass({
                   <CardText>
                     <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                       <div style={{flex: 1}}>
-                        Click link on the right to open your Endpoint in new browser's tab
+                        Click the link on the right to open your Endpoint in a new browser tab.
                       </div>
                       <div style={{paddingLeft: 20}}>
                         <RaisedButton
@@ -107,12 +108,12 @@ export default React.createClass({
                 <Card>
                   <CardTitle title="Use in your App"/>
                   <CardText>
-                    <p>Choose your favorite language below and copy the code</p>
+                    <p>Choose your favorite language below and copy the code.</p>
                     <CodePreview>
                       <CodePreview.Item
                         title="cURL"
                         languageClassName="markup"
-                        code={`curl -X GET\n-H "X-API-KEY: API_KEY"\n"https://api.syncano.io/v1.1/instances/${currentInstance.name}/endpoints/data/${item.name}/get/"`} />
+                        code={`curl -X GET\n-H "X-API-KEY: ${token}"\n"https://api.syncano.io/v1.1/instances/${currentInstance.name}/endpoints/data/${item.name}/get/"`} />
                       <CodePreview.Item
                         title="JavaScript"
                         languageClassName="javascript"
