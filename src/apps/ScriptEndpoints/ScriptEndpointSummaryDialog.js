d6cf9a8107e751d2ff9a6badf9ece5f803a57243
Daniel Siwek <danielsiwek95@gmail.com>
2016-06-17 23:03:32 +0200

    [DASH-1649] Enabled linter

--- src/apps/ScriptEndpoints/ScriptEndpointSummaryDialog.js
+++ src/apps/ScriptEndpoints/ScriptEndpointSummaryDialog.js
@@ -1,24 +1,21 @@
-/* eslint-disable */
 import React from 'react';
-import {Link} from 'react-router';
 import Reflux from 'reflux';
 
 import Store from './ScriptEndpointSummaryDialogStore';
-import ClassesStore from './../Classes/ClassesStore';
 import ScriptEndpointsStore from './ScriptEndpointsStore';
 import SessionStore from '../Session/SessionStore';
 
-import {DialogMixin} from '../../mixins';
-import {CodePreview, Dialog, Notification, Show} from '../../common/';
-import {Card, CardTitle, CardText, RaisedButton} from 'material-ui';
-import {colors as Colors} from 'material-ui/styles/'
+import { DialogMixin } from '../../mixins';
+import { CodePreview, Dialog } from '../../common/';
+import { Card, CardTitle, CardText, RaisedButton } from 'material-ui';
+import { colors as Colors } from 'material-ui/styles/';
 
 export default React.createClass({
   displayName: 'ScriptEndpointSummaryDialog',
 
   contextTypes: {
     params: React.PropTypes.object
-  },  
+  },
 
   mixins: [
     Reflux.connect(Store),
@@ -27,8 +24,7 @@ export default React.createClass({
   ],
 
   render() {
-    const {params} = this.context;
-    const {open, scriptEndpoints} = this.state;
+    const { open, scriptEndpoints } = this.state;
     const item = ScriptEndpointsStore.data.items[0];
     const token = SessionStore.getToken();
     const currentInstance = SessionStore.getInstance();
@@ -38,26 +34,28 @@ export default React.createClass({
         key="dialog"
         ref="dialog"
         title="You've just created a Script Endpoint!"
-        titleStyle={{paddingLeft: 72}}
+        titleStyle={{ paddingLeft: 72 }}
         onRequestClose={this.handleCancel}
         loading={scriptEndpoints.isLoading}
-        open={open}>
-        <div style={{position: 'absolute', top: 0, left: 24}}>
+        open={open}
+      >
+        <div style={{ position: 'absolute', top: 0, left: 24 }}>
           <span
             className="synicon-socket-script-endpoint"
             style={{
               color: Colors.red400,
               fontSize: 32
-            }} />
+            }}
+          />
         </div>
         {!item || !currentInstance || !token || scriptEndpoints.isLoading ? null : (
           <div>
             <Dialog.ContentSection>
               <div className="col-flex-1">
-                <div style={{fontSize: 16, lineHeight: 1.6, color: 'rgba(68,68,68, .8)'}}>
+                <div style={{ fontSize: 16, lineHeight: 1.6, color: 'rgba(68,68,68, .8)' }}>
                   <p>
-                    Script Endpoint you just created can always be modified later. You can find list of your Script 
-                    Endpoints, under Sockets tab in our Dashboard. If a Script Endpoint is public, you can see a 
+                    Script Endpoint you just created can always be modified later. You can find list of your Script
+                    Endpoints, under Sockets tab in our Dashboard. If a Script Endpoint is public, you can see a
                     clip icon in the Public column. You can click it to copy the link.
                   </p>
                 </div>
@@ -68,11 +66,11 @@ export default React.createClass({
                 <Card>
                   <CardTitle title="Preview Data" />
                   <CardText>
-                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                      <div style={{flex: 1}}>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                      <div style={{ flex: 1 }}>
                         Click the link on the right to open your Script Endpoint in a new browser tab.
                       </div>
-                      <div style={{paddingLeft: 20}}>
+                      <div style={{ paddingLeft: 20 }}>
                         <RaisedButton
                           primary={true}
                           label="Open Endpoint in new tab"
@@ -80,7 +78,8 @@ export default React.createClass({
                           target="_blank"
                           href={`
                             ${SYNCANO_BASE_URL.slice(0, -1)}${item.links.self}?api_key=${token}
-                          `} />
+                          `}
+                        />
                       </div>
                     </div>
                   </CardText>
@@ -90,22 +89,28 @@ export default React.createClass({
             <Dialog.ContentSection>
               <div className="col-flex-1">
                 <Card>
-                  <CardTitle title="Use in your App"/>
+                  <CardTitle title="Use in your App" />
                   <CardText>
                     <p>Choose your favorite language below and copy the code.</p>
                     <CodePreview>
                       <CodePreview.Item
                         title="cURL"
                         languageClassName="markup"
-                        code={`curl -X GET\n-H "X-API-KEY: ${token}"\n"https://api.syncano.io/v1.1/instances/${currentInstance.name}/endpoints/scripts/${item.name}"`} />
+                        code={`curl -X GET\n-H "X-API-KEY: ${token}"\n"https://api.syncano.io/v1.1/instances/` +
+                        `${currentInstance.name}/endpoints/scripts/${item.name}"`}
+                      />
                       <CodePreview.Item
                         title="Python"
                         languageClassName="python"
-                        code={`script_endpoint = ScriptEndpoint.please.get(\n  instance_name='${currentInstance.name}',\n  name='${item.name}'\n)\n\nprint(script_endpoint.script)`} />
-                        <CodePreview.Item
+                        code={`script_endpoint = ScriptEndpoint.please.get(\n  instance_name='` +
+                        `${currentInstance.name}',\n  name='${item.name}'\n)\n\nprint(script_endpoint.script)`}
+                      />
+                      <CodePreview.Item
                         title="JavaScript"
                         languageClassName="javascript"
-                        code={`ScriptEndpoint\n  .please()\n  .get({instanceName: '${currentInstance.name}', name: '${item.name}'})\n  .then(function(scriptEndpoint) {});`} />
+                        code={`ScriptEndpoint\n  .please()\n  .get({instanceName: '${currentInstance.name}',` +
+                        `name: '${item.name}'})\n  .then(function(scriptEndpoint){});`}
+                      />
                     </CodePreview>
                   </CardText>
                 </Card>
