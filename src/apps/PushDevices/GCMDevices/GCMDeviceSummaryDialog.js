81a8e426ceefe5fab195185e04a01555aecb1085
Daniel Siwek <danielsiwek95@gmail.com>
2016-06-28 16:51:36 +0200

    [DASH-1661] Change example code

--- src/apps/PushDevices/GCMDevices/GCMDeviceSummaryDialog.js
+++ src/apps/PushDevices/GCMDevices/GCMDeviceSummaryDialog.js
@@ -6,7 +6,7 @@ import GCMDevicesStore from './GCMDevicesStore';
 import SessionStore from '../../Session/SessionStore';
 
 import { DialogMixin } from '../../../mixins';
-import { CodePreview, Dialog } from '../../../common/';
+import { CodePreview, Dialog, Loading } from '../../../common/';
 import { Card, CardTitle, CardText } from 'material-ui';
 import { colors as Colors } from 'material-ui/styles/';
 
@@ -34,28 +34,30 @@ export default React.createClass({
       <Dialog.FullPage
         key="dialog"
         ref="dialog"
-        title="You've just added Android Device!"
+        title={!showSummaryDialog ? "You've just added Android Device!" : ''}
         titleStyle={{ paddingLeft: 72 }}
         onRequestClose={this.handleCancel}
         loading={GCMDevices.isLoading}
         open={open}
       >
-        <div style={{ position: 'absolute', top: 0, left: 24 }}>
-          <span
-            className="synicon-android"
-            style={{
-              backgroundColor: Colors.blue500,
-              color: '#fff',
-              fontSize: 24,
-              borderRadius: '50%',
-              width: '40px',
-              height: '40px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center' }}
-          />
-        </div>
-        {showSummaryDialog ? null : (
+        {!showSummaryDialog && (
+          <div style={{ position: 'absolute', top: 0, left: 24 }}>
+            <span
+              className="synicon-android"
+              style={{
+                backgroundColor: Colors.blue500,
+                color: '#fff',
+                fontSize: 24,
+                borderRadius: '50%',
+                width: '40px',
+                height: '40px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center' }}
+            />
+          </div>
+        )}
+        {showSummaryDialog ? <Loading show={true} /> : (
           <div>
             <Dialog.ContentSection>
               <div className="col-flex-1">
@@ -85,15 +87,18 @@ export default React.createClass({
                       <CodePreview.Item
                         title="Python"
                         languageClassName="python"
-                        code={`gcm_device = GCMDevice.please.get(\n  instance_name='${currentInstance.name}', ` +
-                        `\n  registration_id='${item.registration_id}'\n) \n\ngcm_device.send_message(\n  content={\n` +
-                        "    'environment': 'development',\n    'data': {\n      'one': 1,\n      'two': 2,\n    " +
-                        '}\n  }\n)'}
+                        code={`import syncano\nfrom syncano.models import GCMDevice\n\nsyncano.connect(api_key=` +
+                        `'${token}')\n\ngcm_device = GCMDevice.please.get(\n  instance_name='` +
+                        `${currentInstance.name}', \n  registration_id='${item.registration_id}'\n) \n\n` +
+                        "gcm_device.send_message(\n  content={\n    'environment': 'development',\n    " +
+                        "data': {\n      'one': 1,\n      'two': 2,\n    }\n  }\n)"}
                       />
                       <CodePreview.Item
                         title="JavaScript"
                         languageClassName="javascript"
-                        code={`var query = {\n  instanceName: "${currentInstance.name}",\n  ` +
+                        code={`var Syncano = require('syncano');\nvar connection = Syncano({accountKey: ` +
+                        `'${token}'});\nvar GCMDevice = connection.GCMDevice;\n\n` +
+                        `var query = {\n  instanceName: "${currentInstance.name}",\n  ` +
                         `registration_id: "${item.registration_id}" \n};\n` +
                         'var content = {\n  environment: "development",\n  data: {\n    one: 1,\n    two: 2\n  }\n};' +
                         `\n\n\GCMDevice\n  .please()\n  .sendMessage(query, content)\n  .then(calback);`}
