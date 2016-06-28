202eaa42c04d91ac44db6d0bcd1f18ec2269ce62
Daniel Siwek <danielsiwek95@gmail.com>
2016-06-28 14:11:12 +0200

    [DASH-1710] Change prolong condition

--- src/apps/Instances/Instances.jsx
+++ src/apps/Instances/Instances.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Reflux from 'reflux';
 import { withRouter } from 'react-router';
 import Helmet from 'react-helmet';
+import _ from 'lodash';
 import { colors as Colors } from 'material-ui/styles/';
 
 // Stores and Actions
@@ -37,9 +38,9 @@ const Instances = React.createClass({
     Actions.fetch();
 
     const { prolongDialog } = this.refs;
-    const { search } = this.props.location;
+    const { query } = this.props.location;
 
-    if (prolongDialog && search === '?prolong') {
+    if (prolongDialog && _.has(query, 'prolong')) {
       prolongDialog.show();
     }
   },
