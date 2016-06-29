707af107b44a4408281ab4724236b6554eb43875
Hubert Wesołowski <hwesol13@gmail.com>
2016-06-29 14:46:41 +0200

    [DASH-1704] restore usage fetch via js lib

--- src/apps/Syncano/Actions/Usage.js
+++ src/apps/Syncano/Actions/Usage.js
@@ -1,38 +1,15 @@
-import moment from 'moment';
+// import moment from 'moment';
 
 export default {
   listTotalDailyUsage(instanceName) {
-    const accountKey = this.NewLibConnection.getAccountKey();
-    let config = {
-      method: 'get',
-      baseURL: SYNCANO_BASE_URL,
-      url: '/v1.1/usage/daily/',
-      params: {
-        api_key: accountKey,
-        total: true,
-        start: moment().startOf('month').format('YYYY-MM-DD'),
-        end: moment().endOf('month').format('YYYY-MM-DD')
-      }
-    };
+    const params = instanceName !== 'all' ? { instance: instanceName } : { total: true };
 
-    if (!instanceName || instanceName !== 'all') {
-      config.params.instance = instanceName;
-      delete config.params.total;
-    }
-
-    this.Promise
-      .request(config)
-      .then((response) => this.completed(response.data.objects))
+    this.NewLibConnection
+      .DailyUsage
+      .please()
+      .list({}, params)
+      .currentMonth()
+      .then(this.completed)
       .catch(this.failure);
-
-    // We need to back to library call after DailyUsage will handle custom filtering
-    // this.NewLibConnection
-    //   .DailyUsage
-    //   .please()
-    //   .list({}, { instance: 'empty-dream-2074' })
-    //   .currentMonth()
-    //   .total()
-    //   .then(this.completed)
-    //   .catch(this.failure);
   }
 };
